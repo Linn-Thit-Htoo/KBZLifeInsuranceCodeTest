135bcf4ce67cb5b175f12ee1d82546f30435fdc1
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-04 23:35:55 +0630

    [ Added ] AesEndpoint

---
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Aes/AesEndpoint.cs
@@ -0,0 +1,53 @@
+﻿using KBZLifeInsuranceCodeTest.Shared.Services.SecurityServices;
+using KBZLifeInsuranceCodeTest.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Aes
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AesEndpoint : BaseController
+    {
+        private readonly AesService _aesService;
+
+        public AesEndpoint(AesService aesService)
+        {
+            _aesService = aesService;
+        }
+
+        [HttpPost("encrypt")]
+        public IActionResult Encrypt(string plainText)
+        {
+            Result<string> result;
+            try
+            {
+                var encryptedText = _aesService.Encrypt(plainText);
+                result = Result<string>.Success(data: encryptedText);
+            }
+            catch (Exception ex)
+            {
+                result = Result<string>.Fail(ex);
+            }
+
+            return Content(result);
+        }
+
+        [HttpPost("decrypt")]
+        public IActionResult Decrypt(string encryptedText)
+        {
+            Result<string> result;
+            try
+            {
+                var decryptedText = _aesService.Decrypt(encryptedText);
+                result = Result<string>.Success(data: decryptedText);
+            }
+            catch (Exception ex)
+            {
+                result = Result<string>.Fail(ex);
+            }
+
+            return Content(result);
+        }
+    }
+}
