bd4c22c3f34699812ea38df80d0abbf42a6be07c
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-04 18:14:59 +0630

    [ Added ] LoginEndpoint

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginEndpoint.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginEndpoint.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KBZLifeInsuranceCodeTest.DTOs.Features.Account;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.Login
 {
-    [Route("api/[controller]")]
+    [Route("api/account")]
     [ApiController]
-    public class LoginEndpoint : ControllerBase
+    public class LoginEndpoint : BaseController
     {
+        private readonly IMediator _mediator;
+
+        public LoginEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDTO loginRequest, CancellationToken cs)
+        {
+            var query = new LoginQuery(loginRequest);
+            var result = await _mediator.Send(query, cs);
+
+            return Content(result);
+        }
     }
 }
