0b605f9fdd139ad4afdee134bee3d408412d2e74
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-04 14:30:52 +0630

    [ Added ] CreateAccountCommandHandler

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountCommand.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountCommand.cs
@@ -1,6 +1,18 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount
+﻿using KBZLifeInsuranceCodeTest.DTOs.Features.Account;
+using KBZLifeInsuranceCodeTest.Utils;
+using MediatR;
+
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount
 {
-    public class CreateAccountCommand
+    public class CreateAccountCommand : IRequest<Result<AccountListDTO>>
     {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+
+        public CreateAccountCommand(int pageNo, int pageSize)
+        {
+            PageNo = pageNo;
+            PageSize = pageSize;
+        }
     }
 }
--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountCommandHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountCommandHandler.cs
@@ -1,6 +1,45 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount
+﻿using KBZLifeInsuranceCodeTest.DTOs.Features.Account;
+using KBZLifeInsuranceCodeTest.Utils;
+using KBZLifeInsuranceCodeTest.Utils.Resources;
+using MediatR;
+
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount
 {
-    public class CreateAccountCommandHandler
+    public class CreateAccountCommandHandler : IRequestHandler<CreateAccountCommand, Result<AccountListDTO>>
     {
+        private readonly IAccountRepository _accountRepository;
+
+        public CreateAccountCommandHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<Result<AccountListDTO>> Handle(CreateAccountCommand request, CancellationToken cancellationToken)
+        {
+            Result<AccountListDTO> result;
+            try
+            {
+                if (request.PageNo <= 0)
+                {
+                    result = Result<AccountListDTO>.Fail(MessageResource.InvalidPageNo);
+                    goto result;
+                }
+
+                if (request.PageSize <= 0)
+                {
+                    result = Result<AccountListDTO>.Fail(MessageResource.InvalidPageSize);
+                    goto result;
+                }
+
+                result = await _accountRepository.GetAccountListAsync(request.PageNo, request.PageSize, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                result = Result<AccountListDTO>.Fail(ex);
+            }
+
+        result:
+            return result;
+        }
     }
 }
