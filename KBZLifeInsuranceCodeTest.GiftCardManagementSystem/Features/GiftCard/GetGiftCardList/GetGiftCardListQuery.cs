29cae8934c65f1446d2f3f6d436768cdf6b6bb7d
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-04 23:27:15 +0630

    [ Added ] GetGiftCardListQueryHandler

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardList/GetGiftCardListQuery.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardList/GetGiftCardListQuery.cs
@@ -1,6 +1,18 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardList
+﻿using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
+using KBZLifeInsuranceCodeTest.Utils;
+using MediatR;
+
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardList
 {
-    public class GetGiftCardListQuery
+    public class GetGiftCardListQuery : IRequest<Result<GiftCardListDTO>>
     {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+
+        public GetGiftCardListQuery(int pageNo, int pageSize)
+        {
+            PageNo = pageNo;
+            PageSize = pageSize;
+        }
     }
 }
--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardList/GetGiftCardListQueryHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardList/GetGiftCardListQueryHandler.cs
@@ -1,8 +1,46 @@
 ﻿using KBZLifeInsuranceCodeTest.DbService.AppDbContextModels;
+using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
+using KBZLifeInsuranceCodeTest.Utils;
+using KBZLifeInsuranceCodeTest.Utils.Resources;
+using MediatR;
 
 namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardList
 {
-    public class GetGiftCardListQueryHandler
+    public class GetGiftCardListQueryHandler : IRequestHandler<GetGiftCardListQuery, Result<GiftCardListDTO>>
     {
+        private readonly IGiftCardRepository _giftCardRepository;
+
+        public GetGiftCardListQueryHandler(IGiftCardRepository giftCardRepository)
+        {
+            _giftCardRepository = giftCardRepository;
+        }
+
+        public async Task<Result<GiftCardListDTO>> Handle(GetGiftCardListQuery request, CancellationToken cancellationToken)
+        {
+            Result<GiftCardListDTO> result;
+            try
+            {
+                if (request.PageNo <= 0)
+                {
+                    result = Result<GiftCardListDTO>.Fail(MessageResource.InvalidPageNo);
+                    goto result;
+                }
+
+                if (request.PageSize <= 0)
+                {
+                    result = Result<GiftCardListDTO>.Fail(MessageResource.InvalidPageSize);
+                    goto result;
+                }
+
+                result = await _giftCardRepository.GetGiftCardListAsync(request.PageNo, request.PageSize, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                result = Result<GiftCardListDTO>.Fail(ex);
+            }
+
+        result:
+            return result;
+        }
     }
 }
