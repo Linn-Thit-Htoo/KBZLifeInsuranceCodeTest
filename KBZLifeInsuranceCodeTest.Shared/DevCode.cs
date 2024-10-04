4e99e93e619c23dc4f9af141cb29b759fa44b7de
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-04 12:15:32 +0630

    [ Added ] DapperService

--- KBZLifeInsuranceCodeTest.Shared/DevCode.cs
+++ KBZLifeInsuranceCodeTest.Shared/DevCode.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace KBZLifeInsuranceCodeTest.Shared
 {
-    internal class DevCode
+    public static class DevCode
     {
+        public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj);
+
+        public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
     }
 }
--- KBZLifeInsuranceCodeTest.Shared/Services/DapperService.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/DapperService.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KBZLifeInsuranceCodeTest.Shared.Services
 {
-    internal class DapperService
+    public class DapperService
     {
+        private readonly IConfiguration _configuration;
+
+        public DapperService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<List<T>> QueryAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
+        {
+            try
+            {
+                using IDbConnection db = GetSqlConnection();
+                var lst = await db.QueryAsync<T>(query, parameters, commandType: commandType);
+
+                return lst.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<T> QueryFirstOrDefaultAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
+        {
+            try
+            {
+                using IDbConnection db = GetSqlConnection();
+                var item = await db.QueryFirstOrDefaultAsync<T>(query, parameters, commandType: commandType);
+
+                return item!;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private SqlConnection GetSqlConnection() => new(_configuration.GetConnectionString("DbConnection"));
     }
 }
