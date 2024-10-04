bc58f4afee35c8d0ccf124507aff4bbfa61585ad
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-04 17:57:14 +0630

    [ Added ] CreateAccountEndpoint

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountEndpoint.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountEndpoint.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KBZLifeInsuranceCodeTest.DTOs.Features.Account;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount
 {
-    [Route("api/[controller]")]
+    [Route("api/account")]
     [ApiController]
-    public class CreateAccountEndpoint : ControllerBase
+    public class CreateAccountEndpoint : BaseController
     {
+        private readonly IMediator _mediator;
+
+        public CreateAccountEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAccount([FromBody] AccountRequestDTO accountRequest, CancellationToken cs)
+        {
+            var command = new CreateAccountCommand(accountRequest);
+            var result = await _mediator.Send(command, cs);
+
+            return Content(result);
+        }
     }
 }
