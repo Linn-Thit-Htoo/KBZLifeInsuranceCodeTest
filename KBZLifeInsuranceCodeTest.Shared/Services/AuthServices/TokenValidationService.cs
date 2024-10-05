33e876799000c1dcfea7a167a17a27e359c7bdd6
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:17:43 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/TokenValidationService.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/TokenValidationService.cs
@@ -16,17 +16,22 @@ public class TokenValidationService
             JwtSecurityTokenHandler tokenHandler = new();
             var key = Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Key").Value!);
 
-            TokenValidationParameters parameters = new()
-            {
-                RequireExpirationTime = true,
-                ValidateIssuer = true,
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ValidAudience = _configuration["Jwt:Audience"],
-                ValidIssuer = _configuration["Jwt:Issuer"],
-                IssuerSigningKey = new SymmetricSecurityKey(key)
-            };
-            var principal = tokenHandler.ValidateToken(token, parameters, out SecurityToken securityToken);
+            TokenValidationParameters parameters =
+                new()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidAudience = _configuration["Jwt:Audience"],
+                    ValidIssuer = _configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            var principal = tokenHandler.ValidateToken(
+                token,
+                parameters,
+                out SecurityToken securityToken
+            );
 
             return principal;
         }
