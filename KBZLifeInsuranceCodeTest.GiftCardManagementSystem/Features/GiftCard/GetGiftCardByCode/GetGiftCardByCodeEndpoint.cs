fc1891afe085bb1d5c17fa53494579ed459783d4
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 10:36:07 +0630

    [ Added ] GetGiftCardByCodeEndpoint

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardByCode/GetGiftCardByCodeEndpoint.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardByCode/GetGiftCardByCodeEndpoint.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardByCode
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/gift-card")]
     [ApiController]
-    public class GetGiftCardByCodeEndpoint : ControllerBase
+    public class GetGiftCardByCodeEndpoint : BaseController
     {
+        private readonly IMediator _mediator;
+
+        public GetGiftCardByCodeEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("code/{code}")]
+        public async Task<IActionResult> GetGiftCardByCode(string code, CancellationToken cs)
+        {
+            var query = new GetGiftCardByCodeQuery(code);
+            var result = await _mediator.Send(query, cs);
+
+            return Content(result);
+        }
     }
 }
