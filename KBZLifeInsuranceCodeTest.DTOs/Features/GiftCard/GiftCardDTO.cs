ef0f12c42a7a4a8554e68819cfef5316925826ce
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:11:54 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardDTO.cs
@@ -1,33 +1,32 @@
-﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
+
+public class GiftCardDTO
 {
-    public class GiftCardDTO
-    {
-        public string GiftCardId { get; set; } = null!;
+    public string GiftCardId { get; set; } = null!;
 
-        public string GiftCardNo { get; set; } = null!;
+    public string GiftCardNo { get; set; } = null!;
 
-        public string Title { get; set; } = null!;
+    public string Title { get; set; } = null!;
 
-        public string Description { get; set; } = null!;
+    public string Description { get; set; } = null!;
 
-        public string? ExpiryDate { get; set; }
+    public string? ExpiryDate { get; set; }
 
-        public decimal Amount { get; set; }
+    public decimal Amount { get; set; }
 
-        public string Status { get; set; } = null!;
+    public string Status { get; set; } = null!;
 
-        public DateTime CreatedDate { get; set; }
+    public DateTime CreatedDate { get; set; }
 
-        public DateTime? UpdatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
 
-        public byte[] Qrcode { get; set; } = null!;
+    public byte[] Qrcode { get; set; } = null!;
 
-        public string Duration { get; set; } = null!;
+    public string Duration { get; set; } = null!;
 
-        public bool IsDeleted { get; set; }
+    public bool IsDeleted { get; set; }
 
-        public string? PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
 
-        public int? CashbackPercentage { get; set; }
-    }
+    public int? CashbackPercentage { get; set; }
 }
