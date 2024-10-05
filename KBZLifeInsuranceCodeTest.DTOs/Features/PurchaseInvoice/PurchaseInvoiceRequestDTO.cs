971a3bad5e41b36a7206000818f4f5d48137b74e
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:13:57 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoice/PurchaseInvoiceRequestDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoice/PurchaseInvoiceRequestDTO.cs
@@ -1,10 +1,9 @@
-﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoice
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoice;
+
+public class PurchaseInvoiceRequestDTO
 {
-    public class PurchaseInvoiceRequestDTO
-    {
-        public string UserId { get; set; }
-        public decimal TotalAmount { get; set; }
-        public string PaymentMethod { get; set; }
-        public List<PurchaseInvoiceDetailRequestDTO> PurchaseInvoiceDetailRequests { get; set; }
-    }
+    public string UserId { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string PaymentMethod { get; set; }
+    public List<PurchaseInvoiceDetailRequestDTO> PurchaseInvoiceDetailRequests { get; set; }
 }
