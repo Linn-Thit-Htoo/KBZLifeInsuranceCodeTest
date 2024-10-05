5ba68aa94da14630ad621da87efc1885ad083fd6
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:17:25 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/JwtService.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/JwtService.cs
@@ -15,14 +15,18 @@ public class JwtService
         {
             var claims = new[]
             {
-            new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]!),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
-            new Claim("UserId", jwtResponseModel.UserId),
-            new Claim("UserName", jwtResponseModel.UserName),
-            new Claim("UserRole", jwtResponseModel.UserRole),
-            new Claim(ClaimTypes.Role, jwtResponseModel.UserRole)
-        };
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]!),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(
+                    JwtRegisteredClaimNames.Iat,
+                    new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(),
+                    ClaimValueTypes.Integer64
+                ),
+                new Claim("UserId", jwtResponseModel.UserId),
+                new Claim("UserName", jwtResponseModel.UserName),
+                new Claim("UserRole", jwtResponseModel.UserRole),
+                new Claim(ClaimTypes.Role, jwtResponseModel.UserRole)
+            };
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
             var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
             var token = new JwtSecurityToken(
