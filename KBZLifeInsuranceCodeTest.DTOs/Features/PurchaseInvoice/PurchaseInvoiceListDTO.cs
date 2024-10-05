98c576dfa6430d72f1dfec1f5061769be4cbfcdb
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:13:36 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoice/PurchaseInvoiceListDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoice/PurchaseInvoiceListDTO.cs
@@ -1,11 +1,4 @@
-﻿using KBZLifeInsuranceCodeTest.DTOs.Features.PageSetting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoice
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoice
 {
     public record PurchaseInvoiceListDTO(List<PurchaseInvoiceDTO> PurchaseInvoices, PageSettingDTO PageSetting);
 }
