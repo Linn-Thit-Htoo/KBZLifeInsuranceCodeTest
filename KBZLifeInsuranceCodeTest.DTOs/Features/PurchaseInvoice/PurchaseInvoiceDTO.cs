1c94e4cd90b3690c18cb13887582f984112b19f2
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:13:03 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoice/PurchaseInvoiceDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoice/PurchaseInvoiceDTO.cs
@@ -1,25 +1,24 @@
 ﻿using KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoiceDetail;
 
-namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoice
+namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoice;
+
+public class PurchaseInvoiceDTO
 {
-    public class PurchaseInvoiceDTO
-    {
-        public string PurchaseInvoiceId { get; set; } = null!;
+    public string PurchaseInvoiceId { get; set; } = null!;
 
-        public string UserId { get; set; } = null!;
+    public string UserId { get; set; } = null!;
 
-        public string UserName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
 
-        public string PhoneNumber { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
 
-        public string InvoiceNo { get; set; } = null!;
+    public string InvoiceNo { get; set; } = null!;
 
-        public decimal TotalAmount { get; set; }
+    public decimal TotalAmount { get; set; }
 
-        public DateTime CreatedDate { get; set; }
+    public DateTime CreatedDate { get; set; }
 
-        public string PaymentMethod { get; set; } = null!;
+    public string PaymentMethod { get; set; } = null!;
 
-        public List<PurchaseInvoiceDetailDTO> PurchaseInvoiceDetails { get; set; }
-    }
+    public List<PurchaseInvoiceDetailDTO> PurchaseInvoiceDetails { get; set; }
 }
