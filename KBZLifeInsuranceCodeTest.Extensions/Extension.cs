cdfe40e2012201388d57fa2eae274caa1794b9fe
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:15:43 +0630

    -

--- KBZLifeInsuranceCodeTest.Extensions/Extension.cs
+++ KBZLifeInsuranceCodeTest.Extensions/Extension.cs
@@ -1,64 +1,63 @@
-﻿namespace KBZLifeInsuranceCodeTest.Extensions
+﻿namespace KBZLifeInsuranceCodeTest.Extensions;
+
+public static class Extension
 {
-    public static class Extension
+    public static AccountDTO ToDto(this TblUser tblUser)
     {
-        public static AccountDTO ToDto(this TblUser tblUser)
+        return new AccountDTO
         {
-            return new AccountDTO
-            {
-                UserId = tblUser.UserId,
-                UserName = tblUser.UserName,
-                PhoneNumber = tblUser.PhoneNumber,
-                UserRole = tblUser.UserRole,
-                IsDeleted = tblUser.IsDeleted
-            };
-        }
+            UserId = tblUser.UserId,
+            UserName = tblUser.UserName,
+            PhoneNumber = tblUser.PhoneNumber,
+            UserRole = tblUser.UserRole,
+            IsDeleted = tblUser.IsDeleted
+        };
+    }
 
-        public static TblUser ToEntity(this AccountRequestDTO accountRequest)
+    public static TblUser ToEntity(this AccountRequestDTO accountRequest)
+    {
+        return new TblUser
         {
-            return new TblUser
-            {
-                UserId = Ulid.NewUlid().ToString(),
-                UserName = accountRequest.UserName,
-                PhoneNumber = accountRequest.PhoneNumber,
-                UserRole = accountRequest.UserRole,
-                Password = accountRequest.Password,
-                IsDeleted = false
-            };
-        }
+            UserId = Ulid.NewUlid().ToString(),
+            UserName = accountRequest.UserName,
+            PhoneNumber = accountRequest.PhoneNumber,
+            UserRole = accountRequest.UserRole,
+            Password = accountRequest.Password,
+            IsDeleted = false
+        };
+    }
 
-        public static GiftCardDTO ToDto(this TblGiftcard tblGiftcard)
+    public static GiftCardDTO ToDto(this TblGiftcard tblGiftcard)
+    {
+        return new GiftCardDTO
         {
-            return new GiftCardDTO
-            {
-                Amount = tblGiftcard.Amount,
-                CashbackPercentage = tblGiftcard.CashbackPercentage,
-                CreatedDate = tblGiftcard.CreatedDate,
-                Description = tblGiftcard.Description,
-                ExpiryDate = tblGiftcard.ExpiryDate,
-                GiftCardId = tblGiftcard.GiftCardId,
-                GiftCardNo = tblGiftcard.GiftCardNo,
-                IsDeleted = tblGiftcard.IsDeleted,
-                PhoneNumber = tblGiftcard.PhoneNumber,
-                Qrcode = tblGiftcard.Qrcode,
-                Status = tblGiftcard.Status,
-                Title = tblGiftcard.Title,
-                UpdatedDate = tblGiftcard.UpdatedDate,
-                Duration = $"{tblGiftcard.GiftCardDuration} months"
-            };
-        }
+            Amount = tblGiftcard.Amount,
+            CashbackPercentage = tblGiftcard.CashbackPercentage,
+            CreatedDate = tblGiftcard.CreatedDate,
+            Description = tblGiftcard.Description,
+            ExpiryDate = tblGiftcard.ExpiryDate,
+            GiftCardId = tblGiftcard.GiftCardId,
+            GiftCardNo = tblGiftcard.GiftCardNo,
+            IsDeleted = tblGiftcard.IsDeleted,
+            PhoneNumber = tblGiftcard.PhoneNumber,
+            Qrcode = tblGiftcard.Qrcode,
+            Status = tblGiftcard.Status,
+            Title = tblGiftcard.Title,
+            UpdatedDate = tblGiftcard.UpdatedDate,
+            Duration = $"{tblGiftcard.GiftCardDuration} months"
+        };
+    }
 
-        public static TblPurchaseInvoice ToEntity(this DTOs.Features.PurchaseInvoice.PurchaseInvoiceRequestDTO purchaseInvoiceRequest, decimal totalAmount)
+    public static TblPurchaseInvoice ToEntity(this DTOs.Features.PurchaseInvoice.PurchaseInvoiceRequestDTO purchaseInvoiceRequest, decimal totalAmount)
+    {
+        return new TblPurchaseInvoice
         {
-            return new TblPurchaseInvoice
-            {
-                PurchaseInvoiceId = Ulid.NewUlid().ToString(),
-                UserId = purchaseInvoiceRequest.UserId,
-                InvoiceNo = Ulid.NewUlid().ToString(),
-                CreatedDate = DateTime.UtcNow,
-                TotalAmount = totalAmount,
-                PaymentMethod = purchaseInvoiceRequest.PaymentMethod
-            };
-        }
+            PurchaseInvoiceId = Ulid.NewUlid().ToString(),
+            UserId = purchaseInvoiceRequest.UserId,
+            InvoiceNo = Ulid.NewUlid().ToString(),
+            CreatedDate = DateTime.UtcNow,
+            TotalAmount = totalAmount,
+            PaymentMethod = purchaseInvoiceRequest.PaymentMethod
+        };
     }
 }
