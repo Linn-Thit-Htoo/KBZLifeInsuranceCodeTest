9285d336fe5ffb5bf32d7cbec6cb241f8121fa4f
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:11:28 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/Account/AccountRequestDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/Account/AccountRequestDTO.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.Account;
 
-namespace KBZLifeInsuranceCodeTest.DTOs.Features.Account
+public class AccountRequestDTO
 {
-    public class AccountRequestDTO
-    {
-        public string UserName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
 
-        public string PhoneNumber { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
 
-        public string Password { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
-        public string UserRole { get; set; } = null!;
-    }
+    public string UserRole { get; set; } = null!;
 }
