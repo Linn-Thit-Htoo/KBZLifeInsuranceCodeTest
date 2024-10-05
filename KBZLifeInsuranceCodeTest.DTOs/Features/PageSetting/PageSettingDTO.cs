656475e1a102b9c78be2a674dd2872893f199977
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:12:17 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/PageSetting/PageSettingDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/PageSetting/PageSettingDTO.cs
@@ -1,18 +1,17 @@
-﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.PageSetting
-{
-    public class PageSettingDTO
-    {
-        public int PageNo { get; set; }
-        public int PageSize { get; set; }
-        public int PageCount { get; set; }
-        public int TotalCount { get; set; }
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.PageSetting;
 
-        public PageSettingDTO(int pageNo, int pageSize, int pageCount, int totalCount)
-        {
-            PageNo = pageNo;
-            PageSize = pageSize;
-            PageCount = pageCount;
-            TotalCount = totalCount;
-        }
+public class PageSettingDTO
+{
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+    public int TotalCount { get; set; }
+
+    public PageSettingDTO(int pageNo, int pageSize, int pageCount, int totalCount)
+    {
+        PageNo = pageNo;
+        PageSize = pageSize;
+        PageCount = pageCount;
+        TotalCount = totalCount;
     }
 }
