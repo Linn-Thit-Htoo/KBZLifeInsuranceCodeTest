274c39b7c7d461cc53f61e77321f6364b7fd85a0
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 11:10:08 +0630

    [ Added ] DeactivateGiftCardCommandHandler

---
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardCommand.cs
@@ -0,0 +1,17 @@
+﻿
+using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
+using KBZLifeInsuranceCodeTest.Utils;
+using MediatR;
+
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.DeactivateGiftCard
+{
+    public class DeactivateGiftCardCommand : IRequest<Result<GiftCardDTO>>
+    {
+        public string GiftCardId { get; set; }
+
+        public DeactivateGiftCardCommand(string giftCardId)
+        {
+            GiftCardId = giftCardId;
+        }
+    }
+}
---
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
+using KBZLifeInsuranceCodeTest.Shared;
+using KBZLifeInsuranceCodeTest.Utils;
+using KBZLifeInsuranceCodeTest.Utils.Resources;
+using MediatR;
+
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.DeactivateGiftCard
+{
+    public class DeactivateGiftCardCommandHandler : IRequestHandler<DeactivateGiftCardCommand, Result<GiftCardDTO>>
+    {
+        private readonly IGiftCardRepository _giftCardRepository;
+
+        public DeactivateGiftCardCommandHandler(IGiftCardRepository giftCardRepository)
+        {
+            _giftCardRepository = giftCardRepository;
+        }
+
+        public async Task<Result<GiftCardDTO>> Handle(DeactivateGiftCardCommand request, CancellationToken cancellationToken)
+        {
+            Result<GiftCardDTO> result;
+            try
+            {
+                if (request.GiftCardId.IsNullOrEmpty())
+                {
+                    result = Result<GiftCardDTO>.Fail(MessageResource.InvalidId);
+                    goto result;
+                }
+
+                result = await _giftCardRepository.DeactivateGiftCardAsync(request.GiftCardId, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                result = Result<GiftCardDTO>.Fail(ex);
+            }
+
+        result:
+            return result;
+        }
+    }
+}
