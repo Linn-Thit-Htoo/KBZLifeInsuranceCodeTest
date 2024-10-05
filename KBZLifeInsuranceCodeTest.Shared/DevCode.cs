0b5b1d29b81d78a477dfa7e6ddbacaa568aa570a
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:16:34 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/DevCode.cs
+++ KBZLifeInsuranceCodeTest.Shared/DevCode.cs
@@ -6,22 +6,31 @@ public static class DevCode
 
     public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
 
-    public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> source, int pageNo, int pageSize)
+    public static IQueryable<TSource> Paginate<TSource>(
+        this IQueryable<TSource> source,
+        int pageNo,
+        int pageSize
+    )
     {
         return source.Skip((pageNo - 1) * pageSize).Take(pageSize);
     }
 
-    public static bool IsNullOrEmpty(this string str) => string.IsNullOrWhiteSpace(str) || string.IsNullOrEmpty(str);
+    public static bool IsNullOrEmpty(this string str) =>
+        string.IsNullOrWhiteSpace(str) || string.IsNullOrEmpty(str);
 
     public static string GeneratePromoCode()
     {
         Random random = new Random();
 
         // Generate 6 random digits
-        string digits = new string(Enumerable.Range(0, 6).Select(x => random.Next(0, 10).ToString()[0]).ToArray());
+        string digits = new string(
+            Enumerable.Range(0, 6).Select(x => random.Next(0, 10).ToString()[0]).ToArray()
+        );
 
         // Generate 5 random uppercase letters
-        string alphabets = new string(Enumerable.Range(0, 5).Select(x => (char)random.Next('A', 'Z' + 1)).ToArray());
+        string alphabets = new string(
+            Enumerable.Range(0, 5).Select(x => (char)random.Next('A', 'Z' + 1)).ToArray()
+        );
 
         // Combine the digits and alphabets to create the promo code
         return digits + alphabets;
