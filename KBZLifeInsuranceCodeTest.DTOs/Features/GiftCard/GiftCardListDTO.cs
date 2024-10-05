84a8dcf8672a418bd015ca31ee8d23a42e124b49
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:12:02 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardListDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardListDTO.cs
@@ -1,11 +1,3 @@
-﻿using KBZLifeInsuranceCodeTest.DTOs.Features.PageSetting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
 
-namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard
-{
-    public record GiftCardListDTO(List<GiftCardDTO> GiftCards, PageSettingDTO PageSetting);
-}
+public record GiftCardListDTO(List<GiftCardDTO> GiftCards, PageSettingDTO PageSetting);
