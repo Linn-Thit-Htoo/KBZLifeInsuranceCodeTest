e1cc5b782d14ce7379f8c856386cda84061aee51
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:17:13 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/GlobalUsings.cs
+++ KBZLifeInsuranceCodeTest.Shared/GlobalUsings.cs
@@ -1,1 +1,6 @@
 ﻿global using Newtonsoft.Json;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
--- KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/JwtService.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/JwtService.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-
-namespace KBZLifeInsuranceCodeTest.Shared.Services.AuthServices
+﻿namespace KBZLifeInsuranceCodeTest.Shared.Services.AuthServices
 {
     public class JwtService
     {
