e873915e88d17fcea1829d1402977ad892438473
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 10:34:26 +0630

    [ Aded ] GetGiftCardByCodeQueryHandler

---
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardByCode/GetGiftCardByCodeQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿
+using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
+using KBZLifeInsuranceCodeTest.Shared;
+using KBZLifeInsuranceCodeTest.Utils;
+using MediatR;
+
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardByCode
+{
+    public class GetGiftCardByCodeQueryHandler : IRequestHandler<GetGiftCardByCodeQuery, Result<GiftCardDTO>>
+    {
+        private readonly IGiftCardRepository _giftCardRepository;
+
+        public GetGiftCardByCodeQueryHandler(IGiftCardRepository giftCardRepository)
+        {
+            _giftCardRepository = giftCardRepository;
+        }
+
+        public async Task<Result<GiftCardDTO>> Handle(GetGiftCardByCodeQuery request, CancellationToken cancellationToken)
+        {
+            Result<GiftCardDTO> result;
+            try
+            {
+                if (request.GiftCardCode.IsNullOrEmpty())
+                {
+                    result = Result<GiftCardDTO>.Fail("Invalid Code.");
+                    goto result;
+                }
+
+                result = await _giftCardRepository.GetGiftCardByCodeAsync(request.GiftCardCode, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                result = Result<GiftCardDTO>.Fail(ex);
+            }
+
+        result:
+            return result;
+        }
+    }
+}
