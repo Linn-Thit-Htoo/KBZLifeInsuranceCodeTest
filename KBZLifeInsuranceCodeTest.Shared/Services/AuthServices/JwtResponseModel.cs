48070fd520535859b0d57d962ec59f8bffc33e64
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:16:47 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/JwtResponseModel.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/AuthServices/JwtResponseModel.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace KBZLifeInsuranceCodeTest.Shared.Services.AuthServices;
 
-namespace KBZLifeInsuranceCodeTest.Shared.Services.AuthServices
+public class JwtResponseModel
 {
-    public class JwtResponseModel
+    public string UserId { get; set; }
+
+    public string UserName { get; set; }
+
+    public string UserRole { get; set; }
+
+    public string? Token { get; set; }
+
+    public JwtResponseModel(string userId, string userName, string userRole)
     {
-        public string UserId { get; set; }
-
-        public string UserName { get; set; }
-
-        public string UserRole { get; set; }
-
-        public string? Token { get; set; }
-
-        public JwtResponseModel(string userId, string userName, string userRole)
-        {
-            UserId = userId;
-            UserName = userName;
-            UserRole = userRole;
-        }
+        UserId = userId;
+        UserName = userName;
+        UserRole = userRole;
     }
 }
