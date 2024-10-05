18d8881d59ea9c03fbb075c2ac21d005a19d633a
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 11:21:30 +0630

    [ Added ] DeactivateGiftCardEndpoint

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardEndpoint.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardEndpoint.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.DeactivateGiftCard
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/gift-card")]
     [ApiController]
-    public class DeactivateGiftCardEndpoint : ControllerBase
+    public class DeactivateGiftCardEndpoint : BaseController
     {
+        private readonly IMediator _mediator;
+
+        public DeactivateGiftCardEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPatch("deactivate/{id}")]
+        public async Task<IActionResult> DeactivateGiftCard(string id)
+        {
+            var command = new DeactivateGiftCardCommand(id);
+            var result = await _mediator.Send(command);
+
+            return Content(result);
+        }
     }
 }
