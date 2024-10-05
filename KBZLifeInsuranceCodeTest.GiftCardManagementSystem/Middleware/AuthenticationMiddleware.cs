0ca8961ec13c4547a0c8d3f0a2cb3c751fb22c56
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 00:12:26 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Middleware/AuthenticationMiddleware.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Middleware/AuthenticationMiddleware.cs
@@ -2,78 +2,77 @@
 using KBZLifeInsuranceCodeTest.Shared.Services.AuthServices;
 using KBZLifeInsuranceCodeTest.Utils;
 
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Middleware
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Middleware;
+
+public class AuthenticationMiddleware
 {
-    public class AuthenticationMiddleware
+    private readonly RequestDelegate _next;
+    private readonly TokenValidationService _tokenValidationService;
+
+    public AuthenticationMiddleware(
+        RequestDelegate next,
+        TokenValidationService tokenValidationService
+    )
     {
-        private readonly RequestDelegate _next;
-        private readonly TokenValidationService _tokenValidationService;
+        _next = next;
+        _tokenValidationService = tokenValidationService;
+    }
 
-        public AuthenticationMiddleware(
-            RequestDelegate next,
-            TokenValidationService tokenValidationService
-        )
+    public async Task InvokeAsync(HttpContext context)
+    {
+        Result<string> result;
+        try
         {
-            _next = next;
-            _tokenValidationService = tokenValidationService;
-        }
+            string authHeader = context.Request.Headers.Authorization!;
+            string requestPath = context.Request.Path;
 
-        public async Task InvokeAsync(HttpContext context)
-        {
-            Result<string> result;
-            try
+            if (ShouldPass(requestPath))
             {
-                string authHeader = context.Request.Headers.Authorization!;
-                string requestPath = context.Request.Path;
-
-                if (ShouldPass(requestPath))
-                {
-                    await _next.Invoke(context);
-                    return;
-                }
-
-                if (authHeader is null || !authHeader.StartsWith("Bearer"))
-                {
-                    goto result;
-                }
-
-                string[] header_token = authHeader.Split(" ");
-                string header = header_token[0];
-                string token = header_token[1];
-
-                var principal = _tokenValidationService.ValidateToken(token);
-                if (principal is null)
-                {
-                    goto result;
-                }
-
                 await _next.Invoke(context);
                 return;
             }
-            catch (Exception ex)
+
+            if (authHeader is null || !authHeader.StartsWith("Bearer"))
             {
                 goto result;
             }
 
-            result:
-            result = Result<string>.UnAuthorized();
-            await context.Response.WriteAsync(result.ToJson());
-        }
+            string[] header_token = authHeader.Split(" ");
+            string header = header_token[0];
+            string token = header_token[1];
 
-        private bool ShouldPass(string requestPath)
-        {
-            return GetPublicAccessEndpoints().Any(x => x.Equals(requestPath));
-        }
-
-        private static List<string> GetPublicAccessEndpoints()
-        {
-            return new List<string>()
+            var principal = _tokenValidationService.ValidateToken(token);
+            if (principal is null)
             {
-                "/api/v1/account/register",
-                "/api/v1/account/login",
-                "/api/v1/encrypt",
-                "/api/v1/decrypt"
-            };
+                goto result;
+            }
+
+            await _next.Invoke(context);
+            return;
         }
+        catch (Exception ex)
+        {
+            goto result;
+        }
+
+        result:
+        result = Result<string>.UnAuthorized();
+        await context.Response.WriteAsync(result.ToJson());
+    }
+
+    private bool ShouldPass(string requestPath)
+    {
+        return GetPublicAccessEndpoints().Any(x => x.Equals(requestPath));
+    }
+
+    private static List<string> GetPublicAccessEndpoints()
+    {
+        return new List<string>()
+        {
+            "/api/v1/account/register",
+            "/api/v1/account/login",
+            "/api/v1/encrypt",
+            "/api/v1/decrypt"
+        };
     }
 }
