d8472b5b2288c59c447d41ff874f8188642511c5
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:10:45 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/Account/AccountListDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/Account/AccountListDTO.cs
@@ -1,6 +1,5 @@
 ﻿using KBZLifeInsuranceCodeTest.DTOs.Features.PageSetting;
 
-namespace KBZLifeInsuranceCodeTest.DTOs.Features.Account
-{
-    public record AccountListDTO(List<AccountDTO> Accounts, PageSettingDTO PageSetting);
-}
+namespace KBZLifeInsuranceCodeTest.DTOs.Features.Account;
+
+public record AccountListDTO(List<AccountDTO> Accounts, PageSettingDTO PageSetting);
