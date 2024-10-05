63f899d72b7f78d6b0d2873bc2d0276110d7b1fe
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-05 17:14:24 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoiceDetail/PurchaseInvoiceDetailDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/PurchaseInvoiceDetail/PurchaseInvoiceDetailDTO.cs
@@ -1,39 +1,38 @@
-﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoiceDetail
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoiceDetail;
+
+public class PurchaseInvoiceDetailDTO
 {
-    public class PurchaseInvoiceDetailDTO
-    {
-        public string Id { get; set; } = null!;
+    public string Id { get; set; } = null!;
 
-        public string InvoiceNo { get; set; } = null!;
+    public string InvoiceNo { get; set; } = null!;
 
-        public string GiftCardId { get; set; } = null!;
+    public string GiftCardId { get; set; } = null!;
 
-        public string GiftCardNo { get; set; } = null!;
+    public string GiftCardNo { get; set; } = null!;
 
-        public string GiftCardTitle { get; set; } = null!;
+    public string GiftCardTitle { get; set; } = null!;
 
-        public string ExpiryDate { get; set; } = null!;
+    public string ExpiryDate { get; set; } = null!;
 
-        public decimal Amount { get; set; }
+    public decimal Amount { get; set; }
 
-        public string Status { get; set; } = null!;
+    public string Status { get; set; } = null!;
 
-        public int GiftCardDuration { get; set; }
+    public int GiftCardDuration { get; set; }
 
-        public DateTime CreatedDate { get; set; }
+    public DateTime CreatedDate { get; set; }
 
-        public int CashbackPercentage { get; set; }
+    public int CashbackPercentage { get; set; }
 
-        public int Quantity { get; set; }
+    public int Quantity { get; set; }
 
-        public decimal SubTotal { get; set; }
+    public decimal SubTotal { get; set; }
 
-        public string TypeOfBuying { get; set; } = null!;
+    public string TypeOfBuying { get; set; } = null!;
 
-        public string? RecipientName { get; set; }
+    public string? RecipientName { get; set; }
 
-        public string? RecipientPhoneNumber { get; set; }
+    public string? RecipientPhoneNumber { get; set; }
 
-        public string QRCode { get; set; } = null!;
-    }
+    public string QRCode { get; set; } = null!;
 }
