369e8ad9da6144490baff6b20bda594e53a97bbc
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:14:30 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/Services/DapperService.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/DapperService.cs
@@ -1,13 +1,6 @@
 ﻿using Dapper;
-using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KBZLifeInsuranceCodeTest.Shared.Services
 {
