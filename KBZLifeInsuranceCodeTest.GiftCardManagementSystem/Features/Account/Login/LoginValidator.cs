d27cf4c6ceca1930c3ac016d46b10cfda3ad3a44
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:51:41 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginValidator.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginValidator.cs
@@ -4,12 +4,18 @@ public class LoginValidator : AbstractValidator<LoginRequestDTO>
 {
     public LoginValidator()
     {
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty()
+            .WithMessage("Phone Number cannot be empty.")
+            .NotNull()
+            .WithMessage("Phone Number cannot be null.")
+            .Length(11)
+            .WithMessage("Phone Number is invalid.");
 
-        RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone Number cannot be empty.")
-            .NotNull().WithMessage("Phone Number cannot be null.")
-            .Length(11).WithMessage("Phone Number is invalid.");
-
-        RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty.")
-            .NotNull().WithMessage("Password cannot be null.");
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password cannot be empty.")
+            .NotNull()
+            .WithMessage("Password cannot be null.");
     }
 }
