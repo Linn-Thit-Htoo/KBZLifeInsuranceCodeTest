c6730ddaeda7698624c2862d17977b3ef209d623
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:11:27 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardRequestDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardRequestDTO.cs
@@ -1,9 +1,8 @@
-﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard
-{
-    public class GiftCardRequestDTO
-    {
-        public int? CashbackPercentage { get; set; }
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
 
-        public string? PhoneNumber { get; set; }
-    }
+public class GiftCardRequestDTO
+{
+    public int? CashbackPercentage { get; set; }
+
+    public string? PhoneNumber { get; set; }
 }
