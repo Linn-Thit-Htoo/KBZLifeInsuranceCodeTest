7b9a1c931876a04ed583b728dc90985a0d5e1a57
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:52:40 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/IAccountRepository.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/IAccountRepository.cs
@@ -1,9 +1,8 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account;
+
+public interface IAccountRepository
 {
-    public interface IAccountRepository
-    {
-        Task<Result<AccountListDTO>> GetAccountListAsync(int pageNo, int pageSize, CancellationToken cs);
-        Task<Result<AccountDTO>> CreateAccountAsync(AccountRequestDTO accountRequest, CancellationToken cs);
-        Task<Result<JwtResponseModel>> LoginAsync(LoginRequestDTO loginRequest, CancellationToken cs);
-    }
+    Task<Result<AccountListDTO>> GetAccountListAsync(int pageNo, int pageSize, CancellationToken cs);
+    Task<Result<AccountDTO>> CreateAccountAsync(AccountRequestDTO accountRequest, CancellationToken cs);
+    Task<Result<JwtResponseModel>> LoginAsync(LoginRequestDTO loginRequest, CancellationToken cs);
 }
