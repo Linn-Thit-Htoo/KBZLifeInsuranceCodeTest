9a098dfbf111ad54eec0f6c7471ff6ccd5accb25
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:45:40 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Dependencies/DependencyInjection.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Dependencies/DependencyInjection.cs
@@ -1,21 +1,4 @@
-﻿global using KBZLifeInsuranceCodeTest.DbService.AppDbContextModels;
-global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account;
-global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount;
-global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.Login;
-global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard;
-global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard;
-global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Middleware;
-global using KBZLifeInsuranceCodeTest.Shared.Services;
-global using KBZLifeInsuranceCodeTest.Shared.Services.AuthServices;
-global using KBZLifeInsuranceCodeTest.Shared.Services.CacheServices;
-global using KBZLifeInsuranceCodeTest.Shared.Services.QRServices;
-global using KBZLifeInsuranceCodeTest.Shared.Services.SecurityServices;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.IdentityModel.Tokens;
-global using System.Text;
-
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Dependencies;
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Dependencies;
 
 public static class DependencyInjection
 {
--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/GlobalUsings.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/GlobalUsings.cs
@@ -1,1 +1,16 @@
-﻿
+﻿global using System.Text;
+global using KBZLifeInsuranceCodeTest.DbService.AppDbContextModels;
+global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account;
+global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount;
+global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.Login;
+global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard;
+global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard;
+global using KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Middleware;
+global using KBZLifeInsuranceCodeTest.Shared.Services;
+global using KBZLifeInsuranceCodeTest.Shared.Services.AuthServices;
+global using KBZLifeInsuranceCodeTest.Shared.Services.CacheServices;
+global using KBZLifeInsuranceCodeTest.Shared.Services.QRServices;
+global using KBZLifeInsuranceCodeTest.Shared.Services.SecurityServices;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
