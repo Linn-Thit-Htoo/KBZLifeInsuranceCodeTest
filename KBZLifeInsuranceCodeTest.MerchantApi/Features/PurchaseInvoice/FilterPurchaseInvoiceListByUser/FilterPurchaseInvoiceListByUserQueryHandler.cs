859ae5bab5079a4af248deddaea60bc943435d25
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:06:21 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/FilterPurchaseInvoiceListByUser/FilterPurchaseInvoiceListByUserQueryHandler.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/FilterPurchaseInvoiceListByUser/FilterPurchaseInvoiceListByUserQueryHandler.cs
@@ -1,40 +1,39 @@
-﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.FilterPurchaseInvoiceListByUser
+﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.FilterPurchaseInvoiceListByUser;
+
+public class FilterPurchaseInvoiceListByUserQueryHandler : IRequestHandler<FilterPurchaseInvoiceListByUserQuery, Result<PurchaseInvoiceListDTO>>
 {
-    public class FilterPurchaseInvoiceListByUserQueryHandler : IRequestHandler<FilterPurchaseInvoiceListByUserQuery, Result<PurchaseInvoiceListDTO>>
+    private readonly IPurchaseInvoiceRepository _purchaseInvoiceRepository;
+
+    public FilterPurchaseInvoiceListByUserQueryHandler(IPurchaseInvoiceRepository purchaseInvoiceRepository)
     {
-        private readonly IPurchaseInvoiceRepository _purchaseInvoiceRepository;
+        _purchaseInvoiceRepository = purchaseInvoiceRepository;
+    }
 
-        public FilterPurchaseInvoiceListByUserQueryHandler(IPurchaseInvoiceRepository purchaseInvoiceRepository)
+    public async Task<Result<PurchaseInvoiceListDTO>> Handle(FilterPurchaseInvoiceListByUserQuery request, CancellationToken cancellationToken)
+    {
+        Result<PurchaseInvoiceListDTO> result;
+        try
         {
-            _purchaseInvoiceRepository = purchaseInvoiceRepository;
-        }
-
-        public async Task<Result<PurchaseInvoiceListDTO>> Handle(FilterPurchaseInvoiceListByUserQuery request, CancellationToken cancellationToken)
-        {
-            Result<PurchaseInvoiceListDTO> result;
-            try
+            if (request.UserId.IsNullOrEmpty())
             {
-                if (request.UserId.IsNullOrEmpty())
-                {
-                    result = Result<PurchaseInvoiceListDTO>.Fail(MessageResource.InvalidId);
-                    goto result;
-                }
-
-                if (request.CardStatus.IsNullOrEmpty())
-                {
-                    result = Result<PurchaseInvoiceListDTO>.Fail("Card Status is invalid.");
-                    goto result;
-                }
-
-                result = await _purchaseInvoiceRepository.FilterPurchaseInvoiceListByUserAsyncV1(request.UserId, request.CardStatus, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                result = Result<PurchaseInvoiceListDTO>.Fail(ex);
+                result = Result<PurchaseInvoiceListDTO>.Fail(MessageResource.InvalidId);
+                goto result;
             }
 
-        result:
-            return result;
+            if (request.CardStatus.IsNullOrEmpty())
+            {
+                result = Result<PurchaseInvoiceListDTO>.Fail("Card Status is invalid.");
+                goto result;
+            }
+
+            result = await _purchaseInvoiceRepository.FilterPurchaseInvoiceListByUserAsyncV1(request.UserId, request.CardStatus, cancellationToken);
         }
+        catch (Exception ex)
+        {
+            result = Result<PurchaseInvoiceListDTO>.Fail(ex);
+        }
+
+    result:
+        return result;
     }
 }
