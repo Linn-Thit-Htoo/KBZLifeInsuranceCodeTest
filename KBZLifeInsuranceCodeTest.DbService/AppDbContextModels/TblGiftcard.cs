e4170bd80236eca2499da8532f31a136f66c3305
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:10:36 +0630

    -

--- KBZLifeInsuranceCodeTest.DbService/AppDbContextModels/TblGiftcard.cs
+++ KBZLifeInsuranceCodeTest.DbService/AppDbContextModels/TblGiftcard.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace KBZLifeInsuranceCodeTest.DbService.AppDbContextModels;
+﻿namespace KBZLifeInsuranceCodeTest.DbService.AppDbContextModels;
 
 public partial class TblGiftcard
 {
