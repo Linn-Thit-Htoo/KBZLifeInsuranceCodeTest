e4d5cc5bcf9d46b7f4815100e0b5c0f005e1c917
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:56:40 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/MigrateGiftCard/MigrateGiftCardCommand.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/MigrateGiftCard/MigrateGiftCardCommand.cs
@@ -1,8 +1,4 @@
-﻿using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
-using KBZLifeInsuranceCodeTest.Utils;
-using MediatR;
-
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.MigrateGiftCard
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.MigrateGiftCard
 {
     public class MigrateGiftCardCommand : IRequest<Result<GiftCardDTO>>
     {
