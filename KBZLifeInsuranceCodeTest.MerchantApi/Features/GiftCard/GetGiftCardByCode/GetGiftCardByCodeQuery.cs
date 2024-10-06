2e586dd85f95d4be7ef98ae8d2d65f86e72c525e
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:01:23 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GetGiftCardByCode/GetGiftCardByCodeQuery.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GetGiftCardByCode/GetGiftCardByCodeQuery.cs
@@ -1,15 +1,14 @@
 ﻿using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
 using KBZLifeInsuranceCodeTest.Utils;
 
-namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard.GetGiftCardByCode
-{
-    public class GetGiftCardByCodeQuery : IRequest<Result<GiftCardDTO>>
-    {
-        public string GiftCardCode { get; set; }
+namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard.GetGiftCardByCode;
 
-        public GetGiftCardByCodeQuery(string giftCardCode)
-        {
-            GiftCardCode = giftCardCode;
-        }
+public class GetGiftCardByCodeQuery : IRequest<Result<GiftCardDTO>>
+{
+    public string GiftCardCode { get; set; }
+
+    public GetGiftCardByCodeQuery(string giftCardCode)
+    {
+        GiftCardCode = giftCardCode;
     }
 }
