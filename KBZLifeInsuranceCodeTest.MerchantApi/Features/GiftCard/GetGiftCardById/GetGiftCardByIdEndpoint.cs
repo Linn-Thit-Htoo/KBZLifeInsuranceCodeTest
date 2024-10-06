8b3d64ab2e0addeb8241a7117869ead65e2f7ce0
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:02:23 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GetGiftCardById/GetGiftCardByIdEndpoint.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GetGiftCardById/GetGiftCardByIdEndpoint.cs
@@ -1,23 +1,22 @@
-﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard.GetGiftCardById
+﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard.GetGiftCardById;
+
+[Route("api/v1/gift-card")]
+[ApiController]
+public class GetGiftCardByIdEndpoint : BaseController
 {
-    [Route("api/v1/gift-card")]
-    [ApiController]
-    public class GetGiftCardByIdEndpoint : BaseController
+    private readonly IMediator _mediator;
+
+    public GetGiftCardByIdEndpoint(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public GetGiftCardByIdEndpoint(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetGiftCardById(string id, CancellationToken cs)
+    {
+        var query = new GetGiftCardByIdQuery(id);
+        var result = await _mediator.Send(query, cs);
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetGiftCardById(string id, CancellationToken cs)
-        {
-            var query = new GetGiftCardByIdQuery(id);
-            var result = await _mediator.Send(query, cs);
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
