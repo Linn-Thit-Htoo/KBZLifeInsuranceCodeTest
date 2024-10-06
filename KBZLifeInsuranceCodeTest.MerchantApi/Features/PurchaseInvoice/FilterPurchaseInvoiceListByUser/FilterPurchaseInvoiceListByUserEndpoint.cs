bc94251ed57ed4bab56f305cf6fa53a5e21a8f28
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:05:41 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/FilterPurchaseInvoiceListByUser/FilterPurchaseInvoiceListByUserEndpoint.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/FilterPurchaseInvoiceListByUser/FilterPurchaseInvoiceListByUserEndpoint.cs
@@ -1,23 +1,22 @@
-﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.FilterPurchaseInvoiceListByUser
+﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.FilterPurchaseInvoiceListByUser;
+
+[Route("api/v1/purchase-history")]
+[ApiController]
+public class FilterPurchaseInvoiceListByUserEndpoint : BaseController
 {
-    [Route("api/v1/purchase-history")]
-    [ApiController]
-    public class FilterPurchaseInvoiceListByUserEndpoint : BaseController
+    private readonly IMediator _mediator;
+
+    public FilterPurchaseInvoiceListByUserEndpoint(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public FilterPurchaseInvoiceListByUserEndpoint(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [HttpGet]
+    public async Task<IActionResult> FilterPurchaseHistory(string userId, string cardStatus, CancellationToken cs)
+    {
+        var query = new FilterPurchaseInvoiceListByUserQuery(userId, cardStatus);
+        var result = await _mediator.Send(query, cs);
 
-        [HttpGet]
-        public async Task<IActionResult> FilterPurchaseHistory(string userId, string cardStatus, CancellationToken cs)
-        {
-            var query = new FilterPurchaseInvoiceListByUserQuery(userId, cardStatus);
-            var result = await _mediator.Send(query, cs);
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
