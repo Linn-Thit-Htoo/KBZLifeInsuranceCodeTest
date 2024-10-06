bf68b61af15b970c48d329d6e9c74c162733ba52
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:05:10 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/PaymentMethod/GetPaymentMethodList/GetPaymentMethodListEndpoint.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/PaymentMethod/GetPaymentMethodList/GetPaymentMethodListEndpoint.cs
@@ -1,27 +1,26 @@
 ﻿using KBZLifeInsuranceCodeTest.DTOs.Features.PaymentMethod;
 
-namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PaymentMethod.GetPaymentMethodList
-{
-    [Route("api/v1/payment-method")]
-    [ApiController]
-    public class GetPaymentMethodListEndpoint : BaseController
-    {
-        [HttpGet]
-        public async Task<IActionResult> GetPaymentMethodListAsync(CancellationToken cs)
-        {
-            Result<List<PaymentMethodDTO>> result;
-            try
-            {
-                string jsonStr = await System.IO.File.ReadAllTextAsync("Data/Payment_Method.json", cs);
-                var lst = jsonStr.ToObject<List<PaymentMethodDTO>>();
-                result = Result<List<PaymentMethodDTO>>.Success(lst);
-            }
-            catch (Exception ex)
-            {
-                result = Result<List<PaymentMethodDTO>>.Fail(ex);
-            }
+namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PaymentMethod.GetPaymentMethodList;
 
-            return Content(result);
+[Route("api/v1/payment-method")]
+[ApiController]
+public class GetPaymentMethodListEndpoint : BaseController
+{
+    [HttpGet]
+    public async Task<IActionResult> GetPaymentMethodListAsync(CancellationToken cs)
+    {
+        Result<List<PaymentMethodDTO>> result;
+        try
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("Data/Payment_Method.json", cs);
+            var lst = jsonStr.ToObject<List<PaymentMethodDTO>>();
+            result = Result<List<PaymentMethodDTO>>.Success(lst);
         }
+        catch (Exception ex)
+        {
+            result = Result<List<PaymentMethodDTO>>.Fail(ex);
+        }
+
+        return Content(result);
     }
 }
