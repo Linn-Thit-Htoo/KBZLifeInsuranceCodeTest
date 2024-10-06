0d535997eb8ff5bdde5e6bc7862a80a388dc55d4
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:12:29 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/CommonQuery.cs
+++ KBZLifeInsuranceCodeTest.Shared/CommonQuery.cs
@@ -1,8 +1,7 @@
-﻿namespace KBZLifeInsuranceCodeTest.Shared
+﻿namespace KBZLifeInsuranceCodeTest.Shared;
+
+public class CommonQuery
 {
-    public class CommonQuery
-    {
-        public static string Sp_FilterPurchaseInvoiceByUserId { get; } = "Sp_FilterPurchaseInvoiceByUserId";
-        public static string Sp_GetGiftCardDetailsByInvoiceAndStatus { get; } = "Sp_GetGiftCardDetailsByInvoiceAndStatus";
-    }
+    public static string Sp_FilterPurchaseInvoiceByUserId { get; } = "Sp_FilterPurchaseInvoiceByUserId";
+    public static string Sp_GetGiftCardDetailsByInvoiceAndStatus { get; } = "Sp_GetGiftCardDetailsByInvoiceAndStatus";
 }
