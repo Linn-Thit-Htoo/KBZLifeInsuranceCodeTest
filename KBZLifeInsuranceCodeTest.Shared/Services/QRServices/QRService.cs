aeb7d634536d3ee486f7e4c0860ecea758d05022
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:13:29 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/Services/QRServices/QRService.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/QRServices/QRService.cs
@@ -1,36 +1,35 @@
 ﻿using QRCoder;
 
-namespace KBZLifeInsuranceCodeTest.Shared.Services.QRServices
+namespace KBZLifeInsuranceCodeTest.Shared.Services.QRServices;
+
+public class QRService
 {
-    public class QRService
+    public byte[] GenerateQRCodeByte(string inputText)
     {
-        public byte[] GenerateQRCodeByte(string inputText)
+        try
         {
-            try
-            {
-                using MemoryStream memoryStream = new();
-                QRCodeGenerator qrGenerator = new();
+            using MemoryStream memoryStream = new();
+            QRCodeGenerator qrGenerator = new();
 
-                if (
-                    Uri.TryCreate(
-                        Uri.UnescapeDataString(inputText),
-                        new UriCreationOptions(),
-                        out Uri? result
-                    )
+            if (
+                Uri.TryCreate(
+                    Uri.UnescapeDataString(inputText),
+                    new UriCreationOptions(),
+                    out Uri? result
                 )
-                {
-                    inputText = result.AbsoluteUri;
-                }
-
-                QRCodeData qrCodeData = qrGenerator.CreateQrCode(inputText, QRCodeGenerator.ECCLevel.Q);
-                PngByteQRCode imageByte = new(qrCodeData);
-
-                return imageByte.GetGraphic(50);
-            }
-            catch (Exception ex)
+            )
             {
-                throw new Exception(ex.Message);
+                inputText = result.AbsoluteUri;
             }
+
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(inputText, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode imageByte = new(qrCodeData);
+
+            return imageByte.GetGraphic(50);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }
