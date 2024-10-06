92cfeb82fdd0f795578fcf42a813a690a15916f7
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:15:25 +0630

    -

--- KBZLifeInsuranceCodeTest.Utils/Enums/EnumStatusCode.cs
+++ KBZLifeInsuranceCodeTest.Utils/Enums/EnumStatusCode.cs
@@ -1,15 +1,14 @@
-﻿namespace KBZLifeInsuranceCodeTest.Utils.Enums
+﻿namespace KBZLifeInsuranceCodeTest.Utils.Enums;
+
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        OK = 200,
-        Created = 201,
-        Accepted = 202,
-        NoContent = 204,
-        BadRequest = 400,
-        Unauthorized = 401,
-        NotFound = 404,
-        Conflict = 409,
-        InternalServerError = 500
-    }
+    OK = 200,
+    Created = 201,
+    Accepted = 202,
+    NoContent = 204,
+    BadRequest = 400,
+    Unauthorized = 401,
+    NotFound = 404,
+    Conflict = 409,
+    InternalServerError = 500
 }
