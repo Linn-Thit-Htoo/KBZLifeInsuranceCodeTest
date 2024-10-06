c75ff0dc411dc10fa52c4123b7d8feb4370acc1e
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:15:09 +0630

    -

--- KBZLifeInsuranceCodeTest.Utils/Enums/EnumGiftCardStatus.cs
+++ KBZLifeInsuranceCodeTest.Utils/Enums/EnumGiftCardStatus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KBZLifeInsuranceCodeTest.Utils.Enums
+﻿namespace KBZLifeInsuranceCodeTest.Utils.Enums
 {
     public enum EnumGiftCardStatus
     {
