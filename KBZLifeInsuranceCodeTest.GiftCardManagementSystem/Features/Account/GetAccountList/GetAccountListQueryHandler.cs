7ac71be79d1aa8e2ecd54a5cdab582b26cccb2c4
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:49:58 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/GetAccountList/GetAccountListQueryHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/GetAccountList/GetAccountListQueryHandler.cs
@@ -1,42 +1,41 @@
 ﻿using KBZLifeInsuranceCodeTest.Utils.Resources;
 
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.GetAccountList
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.GetAccountList;
+
+public class GetAccountListQueryHandler : IRequestHandler<GetAccountListQuery, Result<AccountListDTO>>
 {
-    public class GetAccountListQueryHandler : IRequestHandler<GetAccountListQuery, Result<AccountListDTO>>
+    private readonly IAccountRepository _accountRepository;
+
+    public GetAccountListQueryHandler(IAccountRepository accountRepository)
     {
-        private readonly IAccountRepository _accountRepository;
+        _accountRepository = accountRepository;
+    }
 
-        public GetAccountListQueryHandler(IAccountRepository accountRepository)
+    public async Task<Result<AccountListDTO>> Handle(GetAccountListQuery request, CancellationToken cancellationToken)
+    {
+        Result<AccountListDTO> result;
+        try
         {
-            _accountRepository = accountRepository;
-        }
-
-        public async Task<Result<AccountListDTO>> Handle(GetAccountListQuery request, CancellationToken cancellationToken)
-        {
-            Result<AccountListDTO> result;
-            try
+            if (request.PageNo <= 0)
             {
-                if (request.PageNo <= 0)
-                {
-                    result = Result<AccountListDTO>.Fail(MessageResource.InvalidPageNo);
-                    goto result;
-                }
-
-                if (request.PageSize <= 0)
-                {
-                    result = Result<AccountListDTO>.Fail(MessageResource.InvalidPageSize);
-                    goto result;
-                }
-
-                result = await _accountRepository.GetAccountListAsync(request.PageNo, request.PageSize, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                result = Result<AccountListDTO>.Fail(ex);
+                result = Result<AccountListDTO>.Fail(MessageResource.InvalidPageNo);
+                goto result;
             }
 
-        result:
-            return result;
+            if (request.PageSize <= 0)
+            {
+                result = Result<AccountListDTO>.Fail(MessageResource.InvalidPageSize);
+                goto result;
+            }
+
+            result = await _accountRepository.GetAccountListAsync(request.PageNo, request.PageSize, cancellationToken);
         }
+        catch (Exception ex)
+        {
+            result = Result<AccountListDTO>.Fail(ex);
+        }
+
+    result:
+        return result;
     }
 }
