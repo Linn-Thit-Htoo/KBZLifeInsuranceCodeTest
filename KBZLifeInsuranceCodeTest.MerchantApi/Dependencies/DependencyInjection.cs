e2c8d0a8f8a83bbbf1438115073a69e30e45d43f
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:00:04 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Dependencies/DependencyInjection.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Dependencies/DependencyInjection.cs
@@ -11,81 +11,80 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
-namespace KBZLifeInsuranceCodeTest.MerchantApi.Dependencies
+namespace KBZLifeInsuranceCodeTest.MerchantApi.Dependencies;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+        return services.AddDbContextService(builder)
+            .AddMediatRService()
+            .AddAuthenticationService(builder)
+            .AddRepositoryServices()
+            .AddCustomServices()
+            .AddValidatorServices();
+    }
+
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            return services.AddDbContextService(builder)
-                .AddMediatRService()
-                .AddAuthenticationService(builder)
-                .AddRepositoryServices()
-                .AddCustomServices()
-                .AddValidatorServices();
-        }
+            opt.UseMySQL(builder.Configuration.GetConnectionString("DbConnection")!);
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseMySQL(builder.Configuration.GetConnectionString("DbConnection")!);
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+        return services;
+    }
 
-            return services;
-        }
+    private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+    {
+        return services.AddScoped<IGiftCardRepository, GiftCardRepository>()
+            .AddScoped<IPurchaseInvoiceRepository, PurchaseInvoiceRepository>();
+    }
 
-        private static IServiceCollection AddRepositoryServices(this IServiceCollection services)
-        {
-            return services.AddScoped<IGiftCardRepository, GiftCardRepository>()
-                .AddScoped<IPurchaseInvoiceRepository, PurchaseInvoiceRepository>();
-        }
+    private static IServiceCollection AddMediatRService(this IServiceCollection services)
+    {
+        return services.AddMediatR(cf =>
+            cf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly)
+        );
+    }
 
-        private static IServiceCollection AddMediatRService(this IServiceCollection services)
-        {
-            return services.AddMediatR(cf =>
-                cf.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly)
-            );
-        }
-
-        private static IServiceCollection AddAuthenticationService(
-    this IServiceCollection services,
-    WebApplicationBuilder builder
+    private static IServiceCollection AddAuthenticationService(
+this IServiceCollection services,
+WebApplicationBuilder builder
 )
-        {
-            builder
-                .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+    {
+        builder
+            .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
                 {
-                    options.RequireHttpsMetadata = false;
-                    options.SaveToken = true;
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = true,
-                        ValidateLifetime = true,
-                        ValidateAudience = true,
-                        ValidAudience = builder.Configuration["Jwt:Audience"],
-                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
-                        )
-                    };
-                });
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+                    )
+                };
+            });
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddCustomServices(this IServiceCollection services)
-        {
-            return services.AddScoped<AesService>().AddScoped<JwtService>()
-                .AddTransient<TokenValidationService>().AddScoped<DapperService>()
-                .AddScoped<RedisService>();
-        }
+    private static IServiceCollection AddCustomServices(this IServiceCollection services)
+    {
+        return services.AddScoped<AesService>().AddScoped<JwtService>()
+            .AddTransient<TokenValidationService>().AddScoped<DapperService>()
+            .AddScoped<RedisService>();
+    }
 
-        private static IServiceCollection AddValidatorServices(this IServiceCollection services)
-        {
-            return services.AddScoped<PurchaseValidator>();
-        }
+    private static IServiceCollection AddValidatorServices(this IServiceCollection services)
+    {
+        return services.AddScoped<PurchaseValidator>();
     }
 }
