04bc3af309d2083e674bfa9a67996431ae5ce2c1
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:04:13 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GiftCardRepository.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GiftCardRepository.cs
@@ -1,90 +1,89 @@
 ﻿using KBZLifeInsuranceCodeTest.DTOs.Features.PageSetting;
 using KBZLifeInsuranceCodeTest.Extensions;
 
-namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard
+namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard;
+
+public class GiftCardRepository : IGiftCardRepository
 {
-    public class GiftCardRepository : IGiftCardRepository
+    private readonly AppDbContext _context;
+
+    public GiftCardRepository(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public GiftCardRepository(AppDbContext context)
+    public async Task<Result<GiftCardListDTO>> GetGiftCardListAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<GiftCardListDTO> result;
+        try
         {
-            _context = context;
+            var query = _context.TblGiftcards.OrderByDescending(x => x.GiftCardId).Where(x => !x.IsDeleted);
+
+            var lst = await query.Paginate(pageNo, pageSize).ToListAsync(cancellationToken: cs);
+            var totalCount = await query.CountAsync(cancellationToken: cs);
+            var pageCount = totalCount / pageSize;
+
+            if (totalCount % pageSize > 0)
+            {
+                pageCount++;
+            }
+
+            var pageSetting = new PageSettingDTO(pageNo, pageSize, pageCount, totalCount);
+            var model = new GiftCardListDTO(lst.Select(x => x.ToDto()).ToList(), pageSetting);
+            result = Result<GiftCardListDTO>.Success(model);
+        }
+        catch (Exception ex)
+        {
+            result = Result<GiftCardListDTO>.Fail(ex);
         }
 
-        public async Task<Result<GiftCardListDTO>> GetGiftCardListAsync(int pageNo, int pageSize, CancellationToken cs)
+    result:
+        return result;
+    }
+
+    public async Task<Result<GiftCardDTO>> GetGiftCardByIdAsync(string id, CancellationToken cs)
+    {
+        Result<GiftCardDTO> result;
+        try
         {
-            Result<GiftCardListDTO> result;
-            try
+            var item = await _context.TblGiftcards.FirstOrDefaultAsync(x => x.GiftCardId == id && !x.IsDeleted, cancellationToken: cs);
+            if (item is null)
             {
-                var query = _context.TblGiftcards.OrderByDescending(x => x.GiftCardId).Where(x => !x.IsDeleted);
-
-                var lst = await query.Paginate(pageNo, pageSize).ToListAsync(cancellationToken: cs);
-                var totalCount = await query.CountAsync(cancellationToken: cs);
-                var pageCount = totalCount / pageSize;
-
-                if (totalCount % pageSize > 0)
-                {
-                    pageCount++;
-                }
-
-                var pageSetting = new PageSettingDTO(pageNo, pageSize, pageCount, totalCount);
-                var model = new GiftCardListDTO(lst.Select(x => x.ToDto()).ToList(), pageSetting);
-                result = Result<GiftCardListDTO>.Success(model);
-            }
-            catch (Exception ex)
-            {
-                result = Result<GiftCardListDTO>.Fail(ex);
+                result = Result<GiftCardDTO>.NotFound();
+                goto result;
             }
 
-        result:
-            return result;
+            result = Result<GiftCardDTO>.Success(item.ToDto());
+        }
+        catch (Exception ex)
+        {
+            result = Result<GiftCardDTO>.Fail(ex);
         }
 
-        public async Task<Result<GiftCardDTO>> GetGiftCardByIdAsync(string id, CancellationToken cs)
+    result:
+        return result;
+    }
+
+    public async Task<Result<GiftCardDTO>> GetGiftCardByCodeAsync(string giftCartNo, CancellationToken cs)
+    {
+        Result<GiftCardDTO> result;
+        try
         {
-            Result<GiftCardDTO> result;
-            try
+            var item = await _context.TblGiftcards.FirstOrDefaultAsync(x => x.GiftCardNo == giftCartNo && !x.IsDeleted, cancellationToken: cs);
+            if (item is null)
             {
-                var item = await _context.TblGiftcards.FirstOrDefaultAsync(x => x.GiftCardId == id && !x.IsDeleted, cancellationToken: cs);
-                if (item is null)
-                {
-                    result = Result<GiftCardDTO>.NotFound();
-                    goto result;
-                }
-
-                result = Result<GiftCardDTO>.Success(item.ToDto());
-            }
-            catch (Exception ex)
-            {
-                result = Result<GiftCardDTO>.Fail(ex);
+                result = Result<GiftCardDTO>.NotFound();
+                goto result;
             }
 
-        result:
-            return result;
+            result = Result<GiftCardDTO>.Success(item.ToDto());
+        }
+        catch (Exception ex)
+        {
+            result = Result<GiftCardDTO>.Fail(ex);
         }
 
-        public async Task<Result<GiftCardDTO>> GetGiftCardByCodeAsync(string giftCartNo, CancellationToken cs)
-        {
-            Result<GiftCardDTO> result;
-            try
-            {
-                var item = await _context.TblGiftcards.FirstOrDefaultAsync(x => x.GiftCardNo == giftCartNo && !x.IsDeleted, cancellationToken: cs);
-                if (item is null)
-                {
-                    result = Result<GiftCardDTO>.NotFound();
-                    goto result;
-                }
-
-                result = Result<GiftCardDTO>.Success(item.ToDto());
-            }
-            catch (Exception ex)
-            {
-                result = Result<GiftCardDTO>.Fail(ex);
-            }
-
-        result:
-            return result;
-        }
+    result:
+        return result;
     }
 }
