3a46624ebadb516f2acb5a6f2a4c4f4caaae530b
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:52:55 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Aes/AesEndpoint.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Aes/AesEndpoint.cs
@@ -1,48 +1,47 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Aes
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Aes;
+
+[Route("api/v1")]
+[ApiController]
+public class AesEndpoint : BaseController
 {
-    [Route("api/v1")]
-    [ApiController]
-    public class AesEndpoint : BaseController
+    private readonly AesService _aesService;
+
+    public AesEndpoint(AesService aesService)
     {
-        private readonly AesService _aesService;
+        _aesService = aesService;
+    }
 
-        public AesEndpoint(AesService aesService)
+    [HttpPost("encrypt")]
+    public IActionResult Encrypt(string plainText)
+    {
+        Result<string> result;
+        try
         {
-            _aesService = aesService;
+            var encryptedText = _aesService.Encrypt(plainText);
+            result = Result<string>.Success(data: encryptedText);
+        }
+        catch (Exception ex)
+        {
+            result = Result<string>.Fail(ex);
         }
 
-        [HttpPost("encrypt")]
-        public IActionResult Encrypt(string plainText)
-        {
-            Result<string> result;
-            try
-            {
-                var encryptedText = _aesService.Encrypt(plainText);
-                result = Result<string>.Success(data: encryptedText);
-            }
-            catch (Exception ex)
-            {
-                result = Result<string>.Fail(ex);
-            }
+        return Content(result);
+    }
 
-            return Content(result);
+    [HttpPost("decrypt")]
+    public IActionResult Decrypt(string encryptedText)
+    {
+        Result<string> result;
+        try
+        {
+            var decryptedText = _aesService.Decrypt(encryptedText);
+            result = Result<string>.Success(data: decryptedText);
+        }
+        catch (Exception ex)
+        {
+            result = Result<string>.Fail(ex);
         }
 
-        [HttpPost("decrypt")]
-        public IActionResult Decrypt(string encryptedText)
-        {
-            Result<string> result;
-            try
-            {
-                var decryptedText = _aesService.Decrypt(encryptedText);
-                result = Result<string>.Success(data: decryptedText);
-            }
-            catch (Exception ex)
-            {
-                result = Result<string>.Fail(ex);
-            }
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
