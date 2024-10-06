29e9f8cd566a6b4a3ed8d87de323092b3bf0bccd
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:49:31 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/GetAccountList/GetAccountListEndpoint.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/GetAccountList/GetAccountListEndpoint.cs
@@ -1,23 +1,22 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.GetAccountList
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.GetAccountList;
+
+[Route("api/v1/account")]
+[ApiController]
+public class GetAccountListEndpoint : BaseController
 {
-    [Route("api/v1/account")]
-    [ApiController]
-    public class GetAccountListEndpoint : BaseController
+    private readonly IMediator _mediator;
+
+    public GetAccountListEndpoint(IMediator mediator)
     {
-        private readonly IMediator _mediator;
+        _mediator = mediator;
+    }
 
-        public GetAccountListEndpoint(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAccountList(int pageNo, int pageSize, CancellationToken cs)
+    {
+        var query = new GetAccountListQuery(pageNo, pageSize);
+        var result = await _mediator.Send(query, cs);
 
-        [HttpGet]
-        public async Task<IActionResult> GetAccountList(int pageNo, int pageSize, CancellationToken cs)
-        {
-            var query = new GetAccountListQuery(pageNo, pageSize);
-            var result = await _mediator.Send(query, cs);
-
-            return Content(result);
-        }
+        return Content(result);
     }
 }
