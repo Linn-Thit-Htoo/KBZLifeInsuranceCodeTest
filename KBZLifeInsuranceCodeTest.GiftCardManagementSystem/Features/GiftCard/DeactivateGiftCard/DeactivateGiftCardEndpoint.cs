d14e336bb11c6b197eebe5e3242225b677d15f16
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:54:01 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardEndpoint.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardEndpoint.cs
@@ -1,8 +1,4 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.DeactivateGiftCard
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.DeactivateGiftCard
 {
     [Route("api/v1/gift-card")]
     [ApiController]
