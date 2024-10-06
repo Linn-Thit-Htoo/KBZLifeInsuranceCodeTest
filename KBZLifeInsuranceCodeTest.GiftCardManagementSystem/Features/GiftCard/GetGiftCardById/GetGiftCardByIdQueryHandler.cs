b7673a4438dd1539412293255843c2859552f815
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:55:36 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardById/GetGiftCardByIdQueryHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardById/GetGiftCardByIdQueryHandler.cs
@@ -1,10 +1,4 @@
-﻿using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
-using KBZLifeInsuranceCodeTest.Shared;
-using KBZLifeInsuranceCodeTest.Utils;
-using KBZLifeInsuranceCodeTest.Utils.Resources;
-using MediatR;
-
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardById
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardById
 {
     public class GetGiftCardByIdQueryHandler : IRequestHandler<GetGiftCardByIdQuery, Result<GiftCardDTO>>
     {
