d214bb66e2df75384eeefff4dded690274d4d78d
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:57:53 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGiftCardCommandHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGiftCardCommandHandler.cs
@@ -1,10 +1,4 @@
-﻿using KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard;
-using KBZLifeInsuranceCodeTest.Shared;
-using KBZLifeInsuranceCodeTest.Utils;
-using KBZLifeInsuranceCodeTest.Utils.Resources;
-using MediatR;
-
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard
 {
     public class UpdateGiftCardCommandHandler : IRequestHandler<UpdateGiftCardCommand, Result<GiftCardDTO>>
     {
