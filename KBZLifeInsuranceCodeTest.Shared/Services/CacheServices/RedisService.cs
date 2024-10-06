72d7846e2fdf38b1c3d60e2928659949a6f9fd17
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:12:54 +0630

    -

--- KBZLifeInsuranceCodeTest.Shared/Services/CacheServices/RedisService.cs
+++ KBZLifeInsuranceCodeTest.Shared/Services/CacheServices/RedisService.cs
@@ -1,55 +1,54 @@
 ﻿using StackExchange.Redis;
 
-namespace KBZLifeInsuranceCodeTest.Shared.Services.CacheServices
+namespace KBZLifeInsuranceCodeTest.Shared.Services.CacheServices;
+
+public class RedisService
 {
-    public class RedisService
+    private readonly IConfiguration _configuration;
+    private readonly int _redisExpirationLimit;
+
+    public RedisService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-        private readonly int _redisExpirationLimit;
+        _configuration = configuration;
+        _redisExpirationLimit = Convert.ToInt32(_configuration.GetSection("RedisCacheExpirationLimit").Value!);
+    }
 
-        public RedisService(IConfiguration configuration)
+    public async Task SetAsync(string key, object value)
+    {
+        try
         {
-            _configuration = configuration;
-            _redisExpirationLimit = Convert.ToInt32(_configuration.GetSection("RedisCacheExpirationLimit").Value!);
+            var configuration = ConfigurationOptions.Parse("localhost:6379");
+            var redisConnection = await ConnectionMultiplexer.ConnectAsync(configuration);
+
+            var redisCache = redisConnection.GetDatabase();
+            await redisCache.StringSetAsync(key, value.ToJson(), TimeSpan.FromMinutes(_redisExpirationLimit));
         }
-
-        public async Task SetAsync(string key, object value)
+        catch (Exception ex)
         {
-            try
-            {
-                var configuration = ConfigurationOptions.Parse("localhost:6379");
-                var redisConnection = await ConnectionMultiplexer.ConnectAsync(configuration);
-
-                var redisCache = redisConnection.GetDatabase();
-                await redisCache.StringSetAsync(key, value.ToJson(), TimeSpan.FromMinutes(_redisExpirationLimit));
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
+            throw;
         }
+    }
 
-        public async Task<List<T>> GetAsync<T>(string key)
+    public async Task<List<T>> GetAsync<T>(string key)
+    {
+        try
         {
-            try
+            var configuration = ConfigurationOptions.Parse("localhost:6379");
+            var redisConnection = await ConnectionMultiplexer.ConnectAsync(configuration);
+
+            var redisCache = redisConnection.GetDatabase();
+            string? jsonStr = await redisCache.StringGetAsync(key);
+
+            if (jsonStr!.IsNullOrEmpty())
             {
-                var configuration = ConfigurationOptions.Parse("localhost:6379");
-                var redisConnection = await ConnectionMultiplexer.ConnectAsync(configuration);
-
-                var redisCache = redisConnection.GetDatabase();
-                string? jsonStr = await redisCache.StringGetAsync(key);
-
-                if (jsonStr!.IsNullOrEmpty())
-                {
-                    return new List<T>();
-                }
-
-                return jsonStr!.ToObject<List<T>>();
+                return new List<T>();
             }
-            catch (Exception ex)
-            {
-                throw;
-            }
+
+            return jsonStr!.ToObject<List<T>>();
+        }
+        catch (Exception ex)
+        {
+            throw;
         }
     }
 }
