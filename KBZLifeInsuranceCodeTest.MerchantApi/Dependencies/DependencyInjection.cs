aeb4d6e288f02c246ab614df56b7cae26a772395
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:00:28 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Dependencies/DependencyInjection.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Dependencies/DependencyInjection.cs
@@ -1,17 +1,4 @@
-﻿global using KBZLifeInsuranceCodeTest.DbService.AppDbContextModels;
-global using KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard;
-global using KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice;
-global using KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.Purchase;
-global using KBZLifeInsuranceCodeTest.Shared.Services;
-global using KBZLifeInsuranceCodeTest.Shared.Services.AuthServices;
-global using KBZLifeInsuranceCodeTest.Shared.Services.CacheServices;
-global using KBZLifeInsuranceCodeTest.Shared.Services.SecurityServices;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.IdentityModel.Tokens;
-global using System.Text;
-
-namespace KBZLifeInsuranceCodeTest.MerchantApi.Dependencies;
+﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Dependencies;
 
 public static class DependencyInjection
 {
--- KBZLifeInsuranceCodeTest.MerchantApi/GlobalUsings.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/GlobalUsings.cs
@@ -1,1 +1,12 @@
-﻿
+﻿global using System.Text;
+global using KBZLifeInsuranceCodeTest.DbService.AppDbContextModels;
+global using KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard;
+global using KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice;
+global using KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.Purchase;
+global using KBZLifeInsuranceCodeTest.Shared.Services;
+global using KBZLifeInsuranceCodeTest.Shared.Services.AuthServices;
+global using KBZLifeInsuranceCodeTest.Shared.Services.CacheServices;
+global using KBZLifeInsuranceCodeTest.Shared.Services.SecurityServices;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
