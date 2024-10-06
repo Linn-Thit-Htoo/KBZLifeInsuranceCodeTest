c1815e0648e1afe110ffd6bf904c15a675f51b32
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:58:28 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGIftCardValidator.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGIftCardValidator.cs
@@ -1,16 +1,15 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard
-{
-    public class UpdateGiftCardValidator : AbstractValidator<GiftCardRequestDTO>
-    {
-        public UpdateGiftCardValidator()
-        {
-            RuleFor(x => x.CashbackPercentage).GreaterThan(0)
-                .WithMessage("Cashback cannot be empty.")
-                .LessThan(100).WithMessage("Cashback is invalid.");
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard;
 
-            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone Number cannot be empty.")
-                .NotNull().WithMessage("Phone Number cannot be null.")
-                .Length(11).WithMessage("Phone Number is invalid.");
-        }
+public class UpdateGiftCardValidator : AbstractValidator<GiftCardRequestDTO>
+{
+    public UpdateGiftCardValidator()
+    {
+        RuleFor(x => x.CashbackPercentage).GreaterThan(0)
+            .WithMessage("Cashback cannot be empty.")
+            .LessThan(100).WithMessage("Cashback is invalid.");
+
+        RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone Number cannot be empty.")
+            .NotNull().WithMessage("Phone Number cannot be null.")
+            .Length(11).WithMessage("Phone Number is invalid.");
     }
 }
