959cf548ad8d313aa67dfea2a0d0d1ad51ce3c64
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:49:14 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountValidator.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountValidator.cs
@@ -4,17 +4,30 @@ public class CreateAccountValidator : AbstractValidator<AccountRequestDTO>
 {
     public CreateAccountValidator()
     {
-        RuleFor(x => x.UserName).NotEmpty().WithMessage("User Name cannot be empty.")
-            .NotNull().WithMessage("User Name cannot be null.");
+        RuleFor(x => x.UserName)
+            .NotEmpty()
+            .WithMessage("User Name cannot be empty.")
+            .NotNull()
+            .WithMessage("User Name cannot be null.");
 
-        RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone Number cannot be empty.")
-            .NotNull().WithMessage("Phone Number cannot be null.")
-            .Length(11).WithMessage("Phone Number is invalid.");
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty()
+            .WithMessage("Phone Number cannot be empty.")
+            .NotNull()
+            .WithMessage("Phone Number cannot be null.")
+            .Length(11)
+            .WithMessage("Phone Number is invalid.");
 
-        RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty.")
-            .NotNull().WithMessage("Password cannot be null.");
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("Password cannot be empty.")
+            .NotNull()
+            .WithMessage("Password cannot be null.");
 
-        RuleFor(x => x.UserRole).NotEmpty().WithMessage("User Role cannot be empty.")
-            .NotNull().WithMessage("User Role cannot be null.");
+        RuleFor(x => x.UserRole)
+            .NotEmpty()
+            .WithMessage("User Role cannot be empty.")
+            .NotNull()
+            .WithMessage("User Role cannot be null.");
     }
 }
