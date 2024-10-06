01ae7b4fec54c5769c58008a56bef3f289c4c815
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:50:53 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginQuery.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginQuery.cs
@@ -1,9 +1,4 @@
-﻿using KBZLifeInsuranceCodeTest.DTOs.Features.Account;
-using KBZLifeInsuranceCodeTest.Shared.Services.AuthServices;
-using KBZLifeInsuranceCodeTest.Utils;
-using MediatR;
-
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.Login
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.Login
 {
     public class LoginQuery : IRequest<Result<JwtResponseModel>>
     {
