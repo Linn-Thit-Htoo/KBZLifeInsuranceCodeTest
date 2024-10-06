024943b21605b0342b7a38291bfd34bf0dbcb395
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:03:20 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GetGiftCardList/GetGiftCardListEndpoint.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/GetGiftCardList/GetGiftCardListEndpoint.cs
@@ -1,8 +1,4 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard.GetGiftCardList
+﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard.GetGiftCardList
 {
     [Route("api/v1/gift-card")]
     [ApiController]
