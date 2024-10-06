9414d3d591dec5c188201dc202db9ba73fb52984
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:57:58 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGiftCardCommandHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGiftCardCommandHandler.cs
@@ -1,44 +1,43 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard;
+
+public class UpdateGiftCardCommandHandler : IRequestHandler<UpdateGiftCardCommand, Result<GiftCardDTO>>
 {
-    public class UpdateGiftCardCommandHandler : IRequestHandler<UpdateGiftCardCommand, Result<GiftCardDTO>>
+    private readonly IGiftCardRepository _giftCardRepository;
+    private readonly UpdateGiftCardValidator _updateGiftCardValidator;
+
+    public UpdateGiftCardCommandHandler(IGiftCardRepository giftCardRepository, UpdateGiftCardValidator updateGiftCardValidator)
     {
-        private readonly IGiftCardRepository _giftCardRepository;
-        private readonly UpdateGiftCardValidator _updateGiftCardValidator;
+        _giftCardRepository = giftCardRepository;
+        _updateGiftCardValidator = updateGiftCardValidator;
+    }
 
-        public UpdateGiftCardCommandHandler(IGiftCardRepository giftCardRepository, UpdateGiftCardValidator updateGiftCardValidator)
+    public async Task<Result<GiftCardDTO>> Handle(UpdateGiftCardCommand request, CancellationToken cancellationToken)
+    {
+        Result<GiftCardDTO> result;
+        try
         {
-            _giftCardRepository = giftCardRepository;
-            _updateGiftCardValidator = updateGiftCardValidator;
-        }
-
-        public async Task<Result<GiftCardDTO>> Handle(UpdateGiftCardCommand request, CancellationToken cancellationToken)
-        {
-            Result<GiftCardDTO> result;
-            try
+            var validationResult = await _updateGiftCardValidator.ValidateAsync(request.GiftCardRequest, cancellationToken);
+            if (!validationResult.IsValid)
             {
-                var validationResult = await _updateGiftCardValidator.ValidateAsync(request.GiftCardRequest, cancellationToken);
-                if (!validationResult.IsValid)
-                {
-                    string errors = string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage));
-                    result = Result<GiftCardDTO>.Fail(errors);
-                    goto result;
-                }
-
-                if (request.GiftCardId.IsNullOrEmpty())
-                {
-                    result = Result<GiftCardDTO>.Fail(MessageResource.InvalidId);
-                    goto result;
-                }
-
-                result = await _giftCardRepository.UpdateGiftCardAsync(request.GiftCardRequest, request.GiftCardId, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                result = Result<GiftCardDTO>.Fail(ex);
+                string errors = string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage));
+                result = Result<GiftCardDTO>.Fail(errors);
+                goto result;
             }
 
-        result:
-            return result;
+            if (request.GiftCardId.IsNullOrEmpty())
+            {
+                result = Result<GiftCardDTO>.Fail(MessageResource.InvalidId);
+                goto result;
+            }
+
+            result = await _giftCardRepository.UpdateGiftCardAsync(request.GiftCardRequest, request.GiftCardId, cancellationToken);
         }
+        catch (Exception ex)
+        {
+            result = Result<GiftCardDTO>.Fail(ex);
+        }
+
+    result:
+        return result;
     }
 }
