c5f4deb1b38f6f65fe89b6f89f490bbaa64212a5
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:15:36 +0630

    -

--- KBZLifeInsuranceCodeTest.Utils/Enums/EnumTypeOfBuying.cs
+++ KBZLifeInsuranceCodeTest.Utils/Enums/EnumTypeOfBuying.cs
@@ -1,13 +1,12 @@
-﻿namespace KBZLifeInsuranceCodeTest.Utils.Enums
+﻿namespace KBZLifeInsuranceCodeTest.Utils.Enums;
+
+public class EnumTypeOfBuying
 {
-    public class EnumTypeOfBuying
+    public static string GetSelfServiceTypeOfBuying { get; } = "Self Service";
+    public static string GetPresentToOtherTypeOfBuying { get; } = "Present To Other";
+    public static List<string> GetTypeOfBuyingList() => new()
     {
-        public static string GetSelfServiceTypeOfBuying { get; } = "Self Service";
-        public static string GetPresentToOtherTypeOfBuying { get; } = "Present To Other";
-        public static List<string> GetTypeOfBuyingList() => new()
-        {
-            "Self Service",
-            "Present To Other"
-        };
-    }
+        "Self Service",
+        "Present To Other"
+    };
 }
