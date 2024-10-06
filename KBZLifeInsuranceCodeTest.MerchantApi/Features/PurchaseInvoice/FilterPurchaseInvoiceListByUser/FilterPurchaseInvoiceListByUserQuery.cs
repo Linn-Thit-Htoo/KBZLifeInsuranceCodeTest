070b0e2ae0ce2a676ef0c367e0270e83929d761b
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:05:55 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/FilterPurchaseInvoiceListByUser/FilterPurchaseInvoiceListByUserQuery.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/FilterPurchaseInvoiceListByUser/FilterPurchaseInvoiceListByUserQuery.cs
@@ -1,16 +1,15 @@
 ﻿using KBZLifeInsuranceCodeTest.DTOs.Features.PurchaseInvoice;
 
-namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.FilterPurchaseInvoiceListByUser
-{
-    public class FilterPurchaseInvoiceListByUserQuery : IRequest<Result<PurchaseInvoiceListDTO>>
-    {
-        public string UserId { get; set; }
-        public string CardStatus { get; set; }
+namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.FilterPurchaseInvoiceListByUser;
 
-        public FilterPurchaseInvoiceListByUserQuery(string userId, string cardStatus)
-        {
-            UserId = userId;
-            CardStatus = cardStatus;
-        }
+public class FilterPurchaseInvoiceListByUserQuery : IRequest<Result<PurchaseInvoiceListDTO>>
+{
+    public string UserId { get; set; }
+    public string CardStatus { get; set; }
+
+    public FilterPurchaseInvoiceListByUserQuery(string userId, string cardStatus)
+    {
+        UserId = userId;
+        CardStatus = cardStatus;
     }
 }
