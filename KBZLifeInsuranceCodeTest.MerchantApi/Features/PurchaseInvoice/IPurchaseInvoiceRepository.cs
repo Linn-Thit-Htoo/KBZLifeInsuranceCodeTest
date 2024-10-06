f9be899e9e52806da04c690cb503e64beb44a30a
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:08:12 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/IPurchaseInvoiceRepository.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/IPurchaseInvoiceRepository.cs
@@ -2,8 +2,19 @@
 {
     public interface IPurchaseInvoiceRepository
     {
-        Task<Result<PurchaseInvoiceListDTO>> FilterPurchaseInvoiceListByUserAsync(string userId, string cardStatus, CancellationToken cs);
-        Task<Result<PurchaseInvoiceListDTO>> FilterPurchaseInvoiceListByUserAsyncV1(string userId, string cardStatus, CancellationToken cs);
-        Task<Result<PurchaseInvoiceDTO>> MakePaymentAsync(PurchaseInvoiceRequestDTO purchaseInvoiceRequest, CancellationToken cs);
+        Task<Result<PurchaseInvoiceListDTO>> FilterPurchaseInvoiceListByUserAsync(
+            string userId,
+            string cardStatus,
+            CancellationToken cs
+        );
+        Task<Result<PurchaseInvoiceListDTO>> FilterPurchaseInvoiceListByUserAsyncV1(
+            string userId,
+            string cardStatus,
+            CancellationToken cs
+        );
+        Task<Result<PurchaseInvoiceDTO>> MakePaymentAsync(
+            PurchaseInvoiceRequestDTO purchaseInvoiceRequest,
+            CancellationToken cs
+        );
     }
 }
