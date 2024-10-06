0957aaa53867c0f39524c05f6d300d9f3671d365
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:11:22 +0630

    -

--- KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardRequestDTO.cs
+++ KBZLifeInsuranceCodeTest.DTOs/Features/GiftCard/GiftCardRequestDTO.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard
+﻿namespace KBZLifeInsuranceCodeTest.DTOs.Features.GiftCard
 {
     public class GiftCardRequestDTO
     {
