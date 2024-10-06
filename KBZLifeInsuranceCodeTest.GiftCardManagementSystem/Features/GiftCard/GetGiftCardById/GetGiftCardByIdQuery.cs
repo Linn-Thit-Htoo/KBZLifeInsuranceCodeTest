c18537dc8502c4d552688ddfd4185d8c210c64e1
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:55:29 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardById/GetGiftCardByIdQuery.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardById/GetGiftCardByIdQuery.cs
@@ -1,12 +1,11 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardById
-{
-    public class GetGiftCardByIdQuery : IRequest<Result<GiftCardDTO>>
-    {
-        public string GiftCardId { get; set; }
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardById;
 
-        public GetGiftCardByIdQuery(string giftCardId)
-        {
-            GiftCardId = giftCardId;
-        }
+public class GetGiftCardByIdQuery : IRequest<Result<GiftCardDTO>>
+{
+    public string GiftCardId { get; set; }
+
+    public GetGiftCardByIdQuery(string giftCardId)
+    {
+        GiftCardId = giftCardId;
     }
 }
