117a94c61b96aca5104dc4f16bb20aececcc0d15
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:48:53 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountValidator.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountValidator.cs
@@ -1,23 +1,22 @@
 ﻿using FluentValidation;
 
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount;
+
+public class CreateAccountValidator : AbstractValidator<AccountRequestDTO>
 {
-    public class CreateAccountValidator : AbstractValidator<AccountRequestDTO>
+    public CreateAccountValidator()
     {
-        public CreateAccountValidator()
-        {
-            RuleFor(x => x.UserName).NotEmpty().WithMessage("User Name cannot be empty.")
-                .NotNull().WithMessage("User Name cannot be null.");
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("User Name cannot be empty.")
+            .NotNull().WithMessage("User Name cannot be null.");
 
-            RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone Number cannot be empty.")
-                .NotNull().WithMessage("Phone Number cannot be null.")
-                .Length(11).WithMessage("Phone Number is invalid.");
+        RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone Number cannot be empty.")
+            .NotNull().WithMessage("Phone Number cannot be null.")
+            .Length(11).WithMessage("Phone Number is invalid.");
 
-            RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty.")
-                .NotNull().WithMessage("Password cannot be null.");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty.")
+            .NotNull().WithMessage("Password cannot be null.");
 
-            RuleFor(x => x.UserRole).NotEmpty().WithMessage("User Role cannot be empty.")
-                .NotNull().WithMessage("User Role cannot be null.");
-        }
+        RuleFor(x => x.UserRole).NotEmpty().WithMessage("User Role cannot be empty.")
+            .NotNull().WithMessage("User Role cannot be null.");
     }
 }
