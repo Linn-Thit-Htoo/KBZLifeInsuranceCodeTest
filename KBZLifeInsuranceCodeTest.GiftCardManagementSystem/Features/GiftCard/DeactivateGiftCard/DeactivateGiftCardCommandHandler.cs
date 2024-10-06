ff4d21a278024497804b428f6afc13fb58993643
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:53:48 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardCommandHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/DeactivateGiftCard/DeactivateGiftCardCommandHandler.cs
@@ -1,34 +1,33 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.DeactivateGiftCard
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.DeactivateGiftCard;
+
+public class DeactivateGiftCardCommandHandler : IRequestHandler<DeactivateGiftCardCommand, Result<GiftCardDTO>>
 {
-    public class DeactivateGiftCardCommandHandler : IRequestHandler<DeactivateGiftCardCommand, Result<GiftCardDTO>>
+    private readonly IGiftCardRepository _giftCardRepository;
+
+    public DeactivateGiftCardCommandHandler(IGiftCardRepository giftCardRepository)
     {
-        private readonly IGiftCardRepository _giftCardRepository;
+        _giftCardRepository = giftCardRepository;
+    }
 
-        public DeactivateGiftCardCommandHandler(IGiftCardRepository giftCardRepository)
+    public async Task<Result<GiftCardDTO>> Handle(DeactivateGiftCardCommand request, CancellationToken cancellationToken)
+    {
+        Result<GiftCardDTO> result;
+        try
         {
-            _giftCardRepository = giftCardRepository;
-        }
-
-        public async Task<Result<GiftCardDTO>> Handle(DeactivateGiftCardCommand request, CancellationToken cancellationToken)
-        {
-            Result<GiftCardDTO> result;
-            try
+            if (request.GiftCardId.IsNullOrEmpty())
             {
-                if (request.GiftCardId.IsNullOrEmpty())
-                {
-                    result = Result<GiftCardDTO>.Fail(MessageResource.InvalidId);
-                    goto result;
-                }
-
-                result = await _giftCardRepository.DeactivateGiftCardAsync(request.GiftCardId, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                result = Result<GiftCardDTO>.Fail(ex);
+                result = Result<GiftCardDTO>.Fail(MessageResource.InvalidId);
+                goto result;
             }
 
-        result:
-            return result;
+            result = await _giftCardRepository.DeactivateGiftCardAsync(request.GiftCardId, cancellationToken);
         }
+        catch (Exception ex)
+        {
+            result = Result<GiftCardDTO>.Fail(ex);
+        }
+
+    result:
+        return result;
     }
 }
