0aee5c5f9466d33d23d8d2ff30a9bc88d1e90f7a
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:04:58 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/IGiftCardRepository.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/GiftCard/IGiftCardRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard
+﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.GiftCard;
+
+public interface IGiftCardRepository
 {
-    public interface IGiftCardRepository
-    {
-        Task<Result<GiftCardListDTO>> GetGiftCardListAsync(int pageNo, int pageSize, CancellationToken cs);
-        Task<Result<GiftCardDTO>> GetGiftCardByIdAsync(string id, CancellationToken cs);
-        Task<Result<GiftCardDTO>> GetGiftCardByCodeAsync(string giftCartNo, CancellationToken cs);
-    }
+    Task<Result<GiftCardListDTO>> GetGiftCardListAsync(
+        int pageNo,
+        int pageSize,
+        CancellationToken cs
+    );
+    Task<Result<GiftCardDTO>> GetGiftCardByIdAsync(string id, CancellationToken cs);
+    Task<Result<GiftCardDTO>> GetGiftCardByCodeAsync(string giftCartNo, CancellationToken cs);
 }
