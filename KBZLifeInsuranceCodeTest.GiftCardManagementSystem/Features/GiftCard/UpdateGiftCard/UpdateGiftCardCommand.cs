de62618443253c6ddeba79c71462fc98adc3245e
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:57:45 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGiftCardCommand.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/UpdateGiftCard/UpdateGiftCardCommand.cs
@@ -1,14 +1,13 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard
-{
-    public class UpdateGiftCardCommand : IRequest<Result<GiftCardDTO>>
-    {
-        public string GiftCardId { get; set; }
-        public GiftCardRequestDTO GiftCardRequest { get; set; }
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.UpdateGiftCard;
 
-        public UpdateGiftCardCommand(string giftCardId, GiftCardRequestDTO giftCardRequest)
-        {
-            GiftCardId = giftCardId;
-            GiftCardRequest = giftCardRequest;
-        }
+public class UpdateGiftCardCommand : IRequest<Result<GiftCardDTO>>
+{
+    public string GiftCardId { get; set; }
+    public GiftCardRequestDTO GiftCardRequest { get; set; }
+
+    public UpdateGiftCardCommand(string giftCardId, GiftCardRequestDTO giftCardRequest)
+    {
+        GiftCardId = giftCardId;
+        GiftCardRequest = giftCardRequest;
     }
 }
