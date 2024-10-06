cf8f6b8eb6c122eb57a065b0473afb50feb59547
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:16:01 +0630

    -

--- KBZLifeInsuranceCodeTest.Utils/Resources/MessageResource.cs
+++ KBZLifeInsuranceCodeTest.Utils/Resources/MessageResource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KBZLifeInsuranceCodeTest.Utils.Resources
+﻿namespace KBZLifeInsuranceCodeTest.Utils.Resources
 {
     public class MessageResource
     {
