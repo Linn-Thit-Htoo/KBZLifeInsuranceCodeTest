a48598d66b6c2a331142639644d5e258379d34e9
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:16:15 +0630

    -

--- KBZLifeInsuranceCodeTest.Utils/Result.cs
+++ KBZLifeInsuranceCodeTest.Utils/Result.cs
@@ -1,10 +1,5 @@
 ﻿using KBZLifeInsuranceCodeTest.Utils.Enums;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KBZLifeInsuranceCodeTest.Utils
 {
