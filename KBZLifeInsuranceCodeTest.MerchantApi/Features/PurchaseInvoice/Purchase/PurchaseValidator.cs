f801142b0068138682cb790f9872726599c6ca5e
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 13:07:36 +0630

    -

--- KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/Purchase/PurchaseValidator.cs
+++ KBZLifeInsuranceCodeTest.MerchantApi/Features/PurchaseInvoice/Purchase/PurchaseValidator.cs
@@ -1,18 +1,17 @@
-﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.Purchase
+﻿namespace KBZLifeInsuranceCodeTest.MerchantApi.Features.PurchaseInvoice.Purchase;
+
+public class PurchaseValidator : AbstractValidator<PurchaseInvoiceRequestDTO>
 {
-    public class PurchaseValidator : AbstractValidator<PurchaseInvoiceRequestDTO>
+    public PurchaseValidator()
     {
-        public PurchaseValidator()
-        {
-            RuleFor(x => x.UserId).NotEmpty().WithMessage("User ID cannot be empty.")
-                .NotNull().WithMessage("User ID cannot be null.");
+        RuleFor(x => x.UserId).NotEmpty().WithMessage("User ID cannot be empty.")
+            .NotNull().WithMessage("User ID cannot be null.");
 
-            RuleFor(x => x.TotalAmount).NotEmpty().WithMessage("Total Amount cannot be empty.")
-                .NotNull().WithMessage("Total Amount cannot be null.")
-                .GreaterThan(0).WithMessage("Total Amount is invalid.");
+        RuleFor(x => x.TotalAmount).NotEmpty().WithMessage("Total Amount cannot be empty.")
+            .NotNull().WithMessage("Total Amount cannot be null.")
+            .GreaterThan(0).WithMessage("Total Amount is invalid.");
 
-            RuleFor(x => x.PaymentMethod).NotEmpty().WithMessage("Payment Method cannot be empty.")
-                .NotNull().WithMessage("Payment Method cannot be null.");
-        }
+        RuleFor(x => x.PaymentMethod).NotEmpty().WithMessage("Payment Method cannot be empty.")
+            .NotNull().WithMessage("Payment Method cannot be null.");
     }
 }
