7cb31e03e3a7a60eec8ef387e93be7b036f8aecc
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:47:51 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountCommandHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/CreateAccount/CreateAccountCommandHandler.cs
@@ -1,51 +1,66 @@
 ﻿using KBZLifeInsuranceCodeTest.Utils.Enums;
 
-namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount
+namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.Account.CreateAccount;
+
+public class CreateAccountCommandHandler : IRequestHandler<CreateAccountCommand, Result<AccountDTO>>
 {
-    public class CreateAccountCommandHandler : IRequestHandler<CreateAccountCommand, Result<AccountDTO>>
+    private readonly IAccountRepository _accountRepository;
+    private readonly CreateAccountValidator _createAccountValidator;
+    private readonly AesService _aesService;
+
+    public CreateAccountCommandHandler(
+        IAccountRepository accountRepository,
+        CreateAccountValidator createAccountValidator,
+        AesService aesService
+    )
     {
-        private readonly IAccountRepository _accountRepository;
-        private readonly CreateAccountValidator _createAccountValidator;
-        private readonly AesService _aesService;
+        _accountRepository = accountRepository;
+        _createAccountValidator = createAccountValidator;
+        _aesService = aesService;
+    }
 
-        public CreateAccountCommandHandler(IAccountRepository accountRepository, CreateAccountValidator createAccountValidator, AesService aesService)
+    public async Task<Result<AccountDTO>> Handle(
+        CreateAccountCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        Result<AccountDTO> result;
+        try
         {
-            _accountRepository = accountRepository;
-            _createAccountValidator = createAccountValidator;
-            _aesService = aesService;
-        }
-
-        public async Task<Result<AccountDTO>> Handle(CreateAccountCommand request, CancellationToken cancellationToken)
-        {
-            Result<AccountDTO> result;
-            try
+            var validationResult = await _createAccountValidator.ValidateAsync(
+                request.AccountRequest,
+                cancellationToken
+            );
+            if (!validationResult.IsValid)
             {
-                var validationResult = await _createAccountValidator.ValidateAsync(request.AccountRequest, cancellationToken);
-                if (!validationResult.IsValid)
-                {
-                    string errors = string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage));
-                    result = Result<AccountDTO>.Fail(errors);
-                    goto result;
-                }
-
-                if (!Enum.IsDefined(typeof(EnumUserRole), request.AccountRequest.UserRole))
-                {
-                    result = Result<AccountDTO>.Fail("Invalid User Role");
-                    goto result;
-                }
-
-                // key handshake
-                _aesService.Decrypt(request.AccountRequest.Password);
-
-                result = await _accountRepository.CreateAccountAsync(request.AccountRequest, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                result = Result<AccountDTO>.Fail(ex);
+                string errors = string.Join(
+                    " ",
+                    validationResult.Errors.Select(x => x.ErrorMessage)
+                );
+                result = Result<AccountDTO>.Fail(errors);
+                goto result;
             }
 
-        result:
-            return result;
+            if (!Enum.IsDefined(typeof(EnumUserRole), request.AccountRequest.UserRole))
+            {
+                result = Result<AccountDTO>.Fail("Invalid User Role");
+                goto result;
+            }
+
+            // key handshake
+            _aesService.Decrypt(request.AccountRequest.Password);
+
+            result = await _accountRepository.CreateAccountAsync(
+                request.AccountRequest,
+                cancellationToken
+            );
         }
+        catch (Exception ex)
+        {
+            result = Result<AccountDTO>.Fail(ex);
+        }
+
+    result:
+        return result;
     }
 }
