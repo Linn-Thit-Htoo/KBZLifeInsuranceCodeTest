4dee9766a0ddb41583e575d074309e7ba257fcf0
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:51:23 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginQueryHandler.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/Account/Login/LoginQueryHandler.cs
@@ -6,22 +6,35 @@ public class LoginQueryHandler : IRequestHandler<LoginQuery, Result<JwtResponseM
     private readonly LoginValidator _loginValidator;
     private readonly JwtService _jwtService;
 
-    public LoginQueryHandler(IAccountRepository accountRepository, LoginValidator loginValidator, JwtService jwtService)
+    public LoginQueryHandler(
+        IAccountRepository accountRepository,
+        LoginValidator loginValidator,
+        JwtService jwtService
+    )
     {
         _accountRepository = accountRepository;
         _loginValidator = loginValidator;
         _jwtService = jwtService;
     }
 
-    public async Task<Result<JwtResponseModel>> Handle(LoginQuery request, CancellationToken cancellationToken)
+    public async Task<Result<JwtResponseModel>> Handle(
+        LoginQuery request,
+        CancellationToken cancellationToken
+    )
     {
         Result<JwtResponseModel> result;
         try
         {
-            var validationResult = await _loginValidator.ValidateAsync(request.LoginRequest, cancellationToken);
+            var validationResult = await _loginValidator.ValidateAsync(
+                request.LoginRequest,
+                cancellationToken
+            );
             if (!validationResult.IsValid)
             {
-                string errors = string.Join(" ", validationResult.Errors.Select(x => x.ErrorMessage));
+                string errors = string.Join(
+                    " ",
+                    validationResult.Errors.Select(x => x.ErrorMessage)
+                );
                 result = Result<JwtResponseModel>.Fail(errors);
                 goto result;
             }
