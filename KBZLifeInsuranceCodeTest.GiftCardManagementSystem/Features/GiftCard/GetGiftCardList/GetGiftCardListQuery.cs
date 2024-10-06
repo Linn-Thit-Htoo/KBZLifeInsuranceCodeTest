0415e7ca0453345b440638e2c668a8028f429cae
Linn Thit Htoo <lth1212001@gmail.com>
2024-10-06 12:56:15 +0630

    -

--- KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardList/GetGiftCardListQuery.cs
+++ KBZLifeInsuranceCodeTest.GiftCardManagementSystem/Features/GiftCard/GetGiftCardList/GetGiftCardListQuery.cs
@@ -1,14 +1,13 @@
-﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardList
-{
-    public class GetGiftCardListQuery : IRequest<Result<GiftCardListDTO>>
-    {
-        public int PageNo { get; set; }
-        public int PageSize { get; set; }
+﻿namespace KBZLifeInsuranceCodeTest.GiftCardManagementSystem.Features.GiftCard.GetGiftCardList;
 
-        public GetGiftCardListQuery(int pageNo, int pageSize)
-        {
-            PageNo = pageNo;
-            PageSize = pageSize;
-        }
+public class GetGiftCardListQuery : IRequest<Result<GiftCardListDTO>>
+{
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+
+    public GetGiftCardListQuery(int pageNo, int pageSize)
+    {
+        PageNo = pageNo;
+        PageSize = pageSize;
     }
 }
